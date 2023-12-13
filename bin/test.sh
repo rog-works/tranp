a3b74dc7f2b1068b089a3646e7bd343549abddbd
rog-works <rogangelf@gmail.com>
2023-12-14 08:36:48 +0900

    test: テストランナーを改善

--- bin/test.sh
+++ bin/test.sh
@@ -3,5 +3,26 @@
 cwd=$(cd $(dirname $0); pwd)
 appdir=${cwd}/..
 
+target=
+if [ "$1" != "" ]; then
+	target=$(echo "$1" | sed -e 's/\//\./g')
+	target=$(echo "$target" | sed -e 's/\.py$//g')
+fi
+
+if [ "$2" != "" ]; then
+	target=${target}.$2
+fi
+
+if [ "$3" != "" ]; then
+	target=${target}.$3
+fi
+
 source ${cwd}/.env.sh
-python -m unittest discover ${appdir}/tests/
+
+if [ "$target" == "" ]; then
+	echo python -m unittest discover ${appdir}/tests/
+	python -m unittest discover ${appdir}/tests/
+else
+	echo python -m unittest ${target}
+	python -m unittest ${target}
+fi

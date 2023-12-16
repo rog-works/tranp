e5feb8a225f4a83d4ba0c010cf562e3f109bd34f
rog-works <rogangelf@gmail.com>
2023-12-16 20:04:19 +0900

    refactor: dictは入力しないので削除＆コメント追加

--- py2cpp/node/serializer.py
+++ py2cpp/node/serializer.py
@@ -7,6 +7,14 @@ T = TypeVar('T')
 
 
 def serialize(node: Node, schema: type[T]) -> T:
+	"""指定のスキーマを元にノードをdictにシリアライズ
+
+	Args:
+		node (Node): ノード
+		schema (type[T]): シリアライズスキーマ
+	Returns:
+		T: シリアライズしたdict
+	"""
 	out = {
 		in_key: __serialize_value(getattr(node, in_key), in_schema)
 		for in_key, in_schema in schema.__annotations__.items()
@@ -14,15 +22,19 @@ def serialize(node: Node, schema: type[T]) -> T:
 	return cast(T, out)
 
 
-def __serialize_value(value: list[Node] | dict[str, Node] | Node, schema: type) -> Any:
+def __serialize_value(value: list[Node] | Node, schema: type) -> Any:
+	"""指定のスキーマを元にノードをdictにシリアライズ
+
+	Args:
+		value (list[Node] | Node): ノード
+		schema (type[T]): シリアライズスキーマ
+	Returns:
+		Any: シリアライズした値
+	"""
 	if hasattr(schema, '__annotations__') and isinstance(value, Node):
 		return serialize(value, schema)
-	elif hasattr(schema, '__origin__'):
-		origin = getattr(schema, '__origin__')
-		if origin is list and type(value) is list:
-			return [__serialize_value(elem, getattr(schema, '__args__')[0]) for elem in value]
-		elif origin is dict and type(value) is dict:
-			return {key: __serialize_value(value, getattr(schema, '__args__')[1]) for key, value in value.items()}
+	elif hasattr(schema, '__origin__') and getattr(schema, '__origin__') is list and type(value) is list:
+		return [__serialize_value(elem, getattr(schema, '__args__')[0]) for elem in value]
 	elif schema is str and isinstance(value, Node):
 		return value.to_string()
 

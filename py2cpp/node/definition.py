aa817d8875c93e22a8cb09c7b7e8511302192bc3
rog-works <rogangelf@gmail.com>
2023-12-18 13:48:02 +0900

    feat: UnaryOperator実装

--- py2cpp/node/definition.py
+++ py2cpp/node/definition.py
@@ -173,6 +173,25 @@ class DictType(GenericType):
 		return self._by('slices')._at(1).as_a(GenericType)
 
 
+@Meta.embed(Node, actualized(via=Expression))
+class UnaryOperator(Node):
+	@classmethod
+	@override
+	def match_feature(cls, via: Node) -> bool:
+		if via._at(0).to_string() not in ['+', '-', '~']:
+			return False
+
+		return len(via._children()) == 2
+
+	@property
+	def operator(self) -> Terminal:
+		return self._at(0).as_a(Terminal)
+
+	@property
+	def value(self) -> Node:
+		return self._at(1).as_a(Expression).actualize()
+
+
 @Meta.embed(Node, accept_tags('argvalue'))
 class Argument(Node):
 	@property

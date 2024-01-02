0b45b04eb1db27d4969f90cd6022a10b5c5fb2f8
rog-works <rogangelf@gmail.com>
2024-01-02 17:18:45 +0900

    test: SyntaxParserのテスト実装

---
+++ tests/unit/py2cpp/ast/test_parser.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from py2cpp.ast.parser import SyntaxParser
+from py2cpp.lang.annotation import implements
+from tests.unit.py2cpp.ast.test_entry import EntryImpl
+
+
+class SyntaxParserImpl(SyntaxParser):
+	@implements
+	def parse(self, module_path: str) -> EntryImpl:
+		return EntryImpl(('root', []))
+
+
+class TestSyntaxParser(TestCase):
+	def test_parse(self) -> None:
+		self.assertEqual(SyntaxParserImpl().parse('').name, 'root')

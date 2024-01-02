ff13d8aa9be3d80e608d2e1f9c17240d259c65b3
rog-works <rogangelf@gmail.com>
2024-01-02 16:31:38 +0900

    test: EntryCacheのテスト改善

--- tests/unit/py2cpp/ast/test_cache.py
+++ tests/unit/py2cpp/ast/test_cache.py
@@ -1,43 +1,46 @@
-from typing import cast
+from typing import TypeAlias, cast
 from unittest import TestCase
 
-from lark import Token, Tree
-
 from py2cpp.ast.cache import EntryCache
+
+T_Entry: TypeAlias = tuple[str, list] | tuple[str, str]
 
 
 class TestEntryCache(TestCase):
 	def test_exists(self) -> None:
-		cache = EntryCache()
-		cache.add('root', Tree('root', []))
+		cache = EntryCache[T_Entry]()
+		cache.add('root', ('root', []))
 		self.assertEqual(cache.exists('root'), True)
 
 	def test_by(self) -> None:
-		cache = EntryCache()
-		root = Tree('root', [])
+		cache = EntryCache[T_Entry]()
+		root = ('root', [])
 		cache.add('root', root)
 		self.assertEqual(cache.by('root'), root)
 
 	def test_group_by(self) -> None:
-		cache = EntryCache()
-		root = Tree('root', [
-			Token('term_a', ''),
-			Tree('tree_a', [
-				Token('term_b', ''),
+		cache = EntryCache[T_Entry]()
+		root = ('root', [
+			('term_a', ''),
+			('tree_a', [
+				('term_b', ''),
 			]),
-			Token('term_c', ''),
+			('term_c', ''),
 		])
-		tree_a = cast(Tree, root.children[1])
+		tree_a = cast(tuple[str, list], root[1][1])
 		cache.add('root', root)
-		cache.add('root.term_a', root.children[0])
-		cache.add('root.tree_a', root.children[1])
-		cache.add('root.tree_a.term_b', tree_a.children[0])
-		cache.add('root.term_c', root.children[2])
-		arr = list(cache.group_by('root').values())
-		self.assertEqual(arr, [root, root.children[0], tree_a, tree_a.children[0], root.children[2]])
-		self.assertEqual(list(cache.group_by('root.tree_a').values()), [tree_a, tree_a.children[0]])
+		cache.add('root.term_a', root[1][0])
+		cache.add('root.tree_a', root[1][1])
+		cache.add('root.tree_a.term_b', tree_a[1][0])
+		cache.add('root.term_c', root[1][2])
+
+		under_root = list(cache.group_by('root').values())
+		self.assertEqual(under_root, [root, root[1][0], tree_a, tree_a[1][0], root[1][2]])
+
+		under_tree_a = list(cache.group_by('root.tree_a').values())
+		self.assertEqual(under_tree_a, [tree_a, tree_a[1][0]])
 
 	def test_add(self) -> None:
-		cache = EntryCache()
-		cache.add('root', Tree('root', []))
+		cache = EntryCache[T_Entry]()
+		cache.add('root', ('root', []))
 		self.assertEqual(cache.exists('root'), True)

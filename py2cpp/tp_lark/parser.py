c373840d302df299256a4ac8e9b6c3855d21aef3
rog-works <rogangelf@gmail.com>
2024-01-03 10:22:43 +0900

    doc: コメントを追加

--- py2cpp/tp_lark/parser.py
+++ py2cpp/tp_lark/parser.py
@@ -7,24 +7,46 @@ from lark.indenter import PythonIndenter
 
 from py2cpp.ast.entry import Entry
 from py2cpp.ast.parser import ParserSetting, SyntaxParser
-from py2cpp.lang.annotation import implements
+from py2cpp.lang.annotation import implements, injectable
 from py2cpp.lang.cache import CacheProvider
 from py2cpp.lang.io import FileLoader
 from py2cpp.tp_lark.entry import EntryOfLark, Serialization
 
 
 class SyntaxParserOfLark(SyntaxParser):
+	"""シンタックスパーサー(Lark版)"""
+
+	@injectable
 	def __init__(self, loader: FileLoader, setting: ParserSetting, cache: CacheProvider) -> None:
+		"""インスタンスを生成
+
+		Args:
+			loader (FileLoader): ファイルローダー @inject
+			setting (ParserSetting): パーサー設定データ @inject
+			cache (CacheProvider): キャッシュプロバイダー @inject
+		"""
 		self.__loader = loader
 		self.__setting = setting
 		self.__cache = cache
 
 	@implements
 	def parse(self, module_path: str) -> Entry:
+		"""モジュールを解析してシンタックスツリーを生成
+
+		Args:
+			module_path (str): モジュールパス
+		Returns:
+			Entry: シンタックスツリーのルートエントリー
+		"""
 		parser = self.__load_parser()
 		return self.__load_entry(parser, module_path)
 
 	def __load_parser(self) -> Lark:
+		"""シンタックスパーサーをロード
+
+		Returns:
+			Lark: シンタックスパーサー
+		"""
 		def identity() -> dict[str, str]:
 			return {
 				'mtime': str(os.path.getmtime(self.__setting.grammar)),
@@ -45,6 +67,14 @@ class SyntaxParserOfLark(SyntaxParser):
 		return instantiate().lark
 
 	def __load_entry(self, parser: Lark, module_path: str) -> Entry:
+		"""シンタックスツリーをロード
+
+		Args:
+			parser (Lark): シンタックスパーサー
+			module_path (str): モジュールパス
+		Returns:
+			Entry: シンタックスツリーのルートエントリー
+		"""
 		basepath = module_path.replace('.', '/')
 
 		def source_path() -> str:
@@ -63,27 +93,65 @@ class SyntaxParserOfLark(SyntaxParser):
 		return instantiate().entry
 
 class LarkStored:
+	"""ストア(Lark版)"""
+
 	def __init__(self, lark: Lark) -> None:
+		""""インスタンスを生成
+
+		Args:
+			lark (Lark): Larkインスタンス
+		"""
 		self.lark = lark
 
 	@classmethod
 	def load(cls, stream: IO) -> 'LarkStored':
+		""""インスタンスを復元
+
+		Args:
+			stream (IO): IO
+		Returns:
+			LarkStored: インスタンス
+		"""
 		return LarkStored(Lark.load(stream))
 
 	def save(self, stream: IO) -> None:
+		""""インスタンスを保存
+
+		Args:
+			stream (IO): IO
+		"""
 		self.lark.save(stream)
 
 
 class EntryStored:
+	"""ストア(シンタックスツリー版)"""
+
 	def __init__(self, entry: Entry) -> None:
+		""""インスタンスを生成
+
+		Args:
+			lark (Lark): Larkインスタンス
+		"""
 		self.entry = entry
 
 	@classmethod
 	def load(cls, stream: IO) -> 'EntryStored':
+		""""インスタンスを復元
+
+		Args:
+			stream (IO): IO
+		Returns:
+			EntryStored: インスタンス
+		"""
 		data = json.load(stream)
 		tree = cast(Tree, Serialization.loads(data))
 		return EntryStored(EntryOfLark(tree))
 
 	def save(self, stream: IO) -> None:
+		""""インスタンスを保存
+
+		Args:
+			stream (IO): IO
+		"""
 		data = Serialization.dumps(cast(Tree, self.entry.source))
 		stream.write(json.dumps(data).encode('utf-8'))

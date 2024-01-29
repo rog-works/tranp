aacaac2280e57e42be74f33dad5213a980e0a091
rog-works <rogangelf@gmail.com>
2024-01-30 08:36:31 +0900

    doc: コメント追加

--- py2cpp/node/definition/accessor.py
+++ py2cpp/node/definition/accessor.py
@@ -2,6 +2,13 @@ import re
 
 
 def to_access(name: str) -> str:
+	"""名前からアクセス修飾子に変換
+
+	Args:
+		name (str): 名前
+	Returns:
+		str: アクセス修飾子
+	"""
 	if re.fullmatch(r'__.+__', name):
 		return 'public'
 	elif name.startswith('__'):

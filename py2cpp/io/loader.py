aa7455e9c3ca277d78fc403e8461616483eddd2c
rog-works <rogangelf@gmail.com>
2024-01-30 06:47:48 +0900

    doc: コメントを追記

--- py2cpp/io/loader.py
+++ py2cpp/io/loader.py
@@ -1,13 +1,29 @@
 import os
 
-from py2cpp.app.env import Env
+from py2cpp.app.env import Env  # FIXME appに依存するのはNG。インターフェイスと実装の分離を検討
 
 
 class FileLoader:
+	"""ファイルローダー"""
+
 	def __init__(self, env: Env) -> None:
+		"""インスタンスを生成
+
+		Args:
+			env (Env): 環境変数
+		"""
 		self.__env = env
 
 	def load(self, filepath: str) -> str:
+		"""ファイルをロード
+
+		Args:
+			filepath (str): 実行ディレクトリーからの相対パス
+		Returns:
+			str: コンテンツ
+		Raises:
+			FileNotFoundError: 存在しないファイルを指定
+		"""
 		found_filepath = self.__resolve_filepath(filepath)
 		if found_filepath is None:
 			raise FileNotFoundError(f'No such file or directory. filepath: {filepath}')
@@ -16,6 +32,15 @@ class FileLoader:
 			return '\n'.join(f.readlines())
 
 	def mtime(self, filepath: str) -> float:
+		"""ファイルの最終更新日時を取得
+
+		Args:
+			filepath (str): 実行ディレクトリーからの相対パス
+		Returns:
+			float: タイムスタンプ
+		Raises:
+			FileNotFoundError: 存在しないファイルを指定
+		"""
 		found_filepath = self.__resolve_filepath(filepath)
 		if found_filepath is None:
 			raise FileNotFoundError(f'No such file or directory. filepath: {filepath}')
@@ -23,6 +48,15 @@ class FileLoader:
 		return os.path.getmtime(found_filepath)
 
 	def __resolve_filepath(self, filepath: str) -> str | None:
+		"""ファイルパスを解決。未検出の場合はNoneを返却
+
+		Args:
+			filepath (str): 実行ディレクトリーからの相対パス
+		Returns:
+			str | None: 解決したファイルパス
+		Note:
+			環境変数のPYTHONPATHの登録順に探索
+		"""
 		for path in self.__env.paths:
 			abs_filepath = os.path.abspath(os.path.join(path, filepath))
 			if os.path.isfile(abs_filepath):

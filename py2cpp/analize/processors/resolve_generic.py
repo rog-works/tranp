d4a4eccf9d3216cea5eec233750954786f8a1f2b
rog-works <rogangelf@gmail.com>
2024-02-02 19:22:56 +0900

    doc: コメント追記

--- py2cpp/analize/processors/resolve_generic.py
+++ py2cpp/analize/processors/resolve_generic.py
@@ -2,7 +2,6 @@ from py2cpp.analize.symbol import SymbolRaw, SymbolRaws
 from py2cpp.ast.dsn import DSN
 from py2cpp.errors import LogicError
 from py2cpp.lang.implementation import injectable
-from py2cpp.module.modules import Modules
 import py2cpp.node.definition as defs
 
 
@@ -10,11 +9,10 @@ class ResolveGeneric:
 	"""Genericのシンボルを解決"""
 
 	@injectable
-	def __call__(self, modules: Modules, raws: SymbolRaws) -> SymbolRaws:
+	def __call__(self, raws: SymbolRaws) -> SymbolRaws:
 		"""シンボルテーブルを生成
 
 		Args:
-			modules (Modules): モジュールマネージャー @inject
 			raws (SymbolRaws): シンボルテーブル
 		Returns:
 			SymbolRaws: シンボルテーブル
@@ -23,11 +21,18 @@ class ResolveGeneric:
 		for key, raw in raws.items():
 			fore_type = self.__analyze_fore_type(raw)
 			if isinstance(fore_type, defs.GenericType):
-				update_raws[key] = self.__expand_generic(raws, raw, fore_type)
+				update_raws[key] = self.__actualize_generic(raws, raw, fore_type)
 
 		return {**raws, **update_raws}
 
 	def __analyze_fore_type(self, raw: SymbolRaw) -> defs.Type | defs.ClassDef | None:
+		"""シンボルの外形タイプを解析
+
+		Args:
+			raw (SymbolRaw): シンボル
+		Returns:
+			Type | ClassDef | None: 外形タイプ
+		"""
 		if isinstance(raw.decl, (defs.AnnoAssign, defs.Catch)):
 			return raw.decl.var_type
 		elif isinstance(raw.decl, defs.Parameter):
@@ -41,18 +46,35 @@ class ResolveGeneric:
 			# 型指定が無いため全てUnknown
 			return None
 
-	def __expand_generic(self, raws: SymbolRaws, via: SymbolRaw, generic_type: defs.GenericType) -> SymbolRaw:
-			attrs: list[SymbolRaw] = []
-			for t_type in generic_type.template_types:
-				t_raw = self.__resolve_raw(raws, t_type)
-				if isinstance(t_raw.types, defs.GenericType):
-					attrs.append(self.__expand_generic(raws, t_raw, t_raw.types))
-				else:
-					attrs.append(t_raw)
+	def __actualize_generic(self, raws: SymbolRaws, via: SymbolRaw, generic_type: defs.GenericType) -> SymbolRaw:
+		"""ジェネリックタイプノードを解析し、実際の型を取り込んだシンボルに変換
 
-			return via.extends(*attrs)
+		Args:
+			raws (SymbolRaws): シンボルテーブル
+			via: (SymbolRaw): シンボル
+			generic_type (GenericType): ジェネリックタイプノード
+		Returns:
+			SymbolRaw: 変換後のシンボル
+		"""
+		attrs: list[SymbolRaw] = []
+		for t_type in generic_type.template_types:
+			t_raw = self.__resolve_raw(raws, t_type)
+			if isinstance(t_raw.types, defs.GenericType):
+				attrs.append(self.__actualize_generic(raws, t_raw, t_raw.types))
+			else:
+				attrs.append(t_raw)
+
+		return via.extends(*attrs)
 
 	def __resolve_raw(self, raws: SymbolRaws, domain_type: defs.Type) -> SymbolRaw:
+		"""タイプノードからシンボルを解決
+
+		Args:
+			raws (SymbolRaws): シンボルテーブル
+			domain_type: (Type): タイプノード
+		Returns:
+			SymbolRaw: シンボル
+		"""
 		scopes = [DSN.left(domain_type.scope, DSN.elem_counts(domain_type.scope) - i) for i in range(DSN.elem_counts(domain_type.scope))]
 		candidates = [DSN.join(scope, domain_type.domain_name) for scope in scopes]
 		for candidate in candidates:

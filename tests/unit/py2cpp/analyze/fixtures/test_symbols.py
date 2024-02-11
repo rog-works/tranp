0169793527df93c5a0beb02a98aa8a35259c5489
rog-works <rogangelf@gmail.com>
2024-02-11 14:35:27 +0900

    test: テスト改善2

--- tests/unit/py2cpp/analyze/fixtures/test_symbols.py
+++ tests/unit/py2cpp/analyze/fixtures/test_symbols.py
@@ -2,34 +2,34 @@ from typing import TypeAlias
 
 from tests.unit.py2cpp.analyze.fixtures.test_db_xyz import Z
 
-v: int = 0
+value: int = 0
 
 class Base(Z):
 	def __init__(self) -> None:
-		self.s: str = ''
+		self.base_str: str = ''
 
 class Sub(Base):
-	class B2:
-		v: str = ''
+	class C:
+		value: str = ''
 
 		@classmethod
 		def class_func(cls) -> dict[str, int]:
-			return {cls.v: v}
+			return {cls.value: value}
 
 	def __init__(self) -> None:
 		super().__init__()
-		self.v: list[int] = []
+		self.numbers: list[int] = []
 
-	def func1(self, b: 'list[Sub]') -> str:
-		v = False
-		print(v)
-		print(self.v)
-		print(b[0].v)
-		Sub.B2.v = 'b.b2.v'
+	def func1(self, subs: 'list[Sub]') -> str:
+		value = False
+		print(value)
+		print(self.numbers)
+		print(subs[0].numbers)
+		Sub.C.value = 'b.b2.v'
 		self.x.nx = 2
-		v2 = self.v.pop()
-		print(self.func3()[0].v)
-		return self.s
+		v2 = self.numbers.pop()
+		print(self.func3()[0].numbers)
+		return self.base_str
 
 	def func2(self) -> int:
 		a = 1
@@ -42,7 +42,7 @@ class Sub(Base):
 					raise e
 
 		def closure() -> list[int]:
-			b = self.v
+			b = self.numbers
 			return b
 
 		return closure()[0]
@@ -55,7 +55,7 @@ DSI2: TypeAlias = dict[str, DSI]
 Z2: TypeAlias = Z
 
 def func(z2: Z2) -> None:
-	d: DSI = {'s': v}
+	d: DSI = {'s': value}
 	d_in_v = d['s']
 
 	d2: DSI2 = {'s2': d}

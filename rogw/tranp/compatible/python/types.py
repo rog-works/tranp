d819b5bbec1c7f17e2a579d9a8664e35863ce14f
rog-works <rogangelf@gmail.com>
2024-02-20 11:07:12 +0900

    feat: Unionの対応

--- rogw/tranp/compatible/python/types.py
+++ rogw/tranp/compatible/python/types.py
@@ -1,5 +1,6 @@
+from types import UnionType
 from typing import TypeAlias
 
 from rogw.tranp.compatible.python.classes import Pair, Unknown
 
-Primitives: TypeAlias = int | float | str | bool | tuple | list | dict | Pair | Unknown
+Primitives: TypeAlias = int | float | str | bool | tuple | list | dict | UnionType | Pair | Unknown

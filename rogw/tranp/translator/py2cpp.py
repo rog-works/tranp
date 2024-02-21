bddd99404c6dac4c4c4fd614af13ccaa96da908f
rog-works <rogangelf@gmail.com>
2024-02-21 12:49:45 +0900

    fix: on_classの修正

--- rogw/tranp/translator/py2cpp.py
+++ rogw/tranp/translator/py2cpp.py
@@ -243,7 +243,7 @@ class Py2Cpp(Procedure[str]):
 		closure_vars = {'binded_this': node.binded_this}
 		return self.view.render(node.classification, vars={**function_vars, **closure_vars})
 
-	def on_class(self, node: defs.Class, symbol: str, decorators: list[str], inherits: list[str], comment: str, statements: list[str]) -> str:
+	def on_class(self, node: defs.Class, symbol: str, decorators: list[str], inherits: list[str], generic_types: list[str], comment: str, statements: list[str]) -> str:
 		# XXX メンバー変数の展開方法を検討
 		vars: list[str] = []
 		for var in node.class_vars:
@@ -258,8 +258,7 @@ class Py2Cpp(Procedure[str]):
 			this_var_vars = {'is_static': False, 'access': defs.to_access(this_var_name), 'symbol': this_var_name, 'var_type': var_type}
 			vars.append(self.view.render('class_decl_var', vars=this_var_vars))
 
-		generic_types = [generic_type.domain_name for generic_type in node.generic_types]
-		class_vars = {'symbol': symbol, 'decorators': decorators, 'inherits': inherits, 'comment': comment, 'statements': statements, 'vars': vars, 'generic_types': generic_types}
+		class_vars = {'symbol': symbol, 'decorators': decorators, 'inherits': inherits, 'generic_types': generic_types, 'comment': comment, 'statements': statements, 'vars': vars}
 		return self.view.render(node.classification, vars=class_vars)
 
 	def on_enum(self, node: defs.Enum, symbol: str, decorators: list[str], inherits: list[str], comment: str, statements: list[str]) -> str:

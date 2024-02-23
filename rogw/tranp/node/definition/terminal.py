27f96b54ca3a5a4a80ebab2256953f01ebe31716
rog-works <rogangelf@gmail.com>
2024-02-23 18:37:23 +0900

    fix: 比較演算子の展開を抑制

--- rogw/tranp/node/definition/terminal.py
+++ rogw/tranp/node/definition/terminal.py
@@ -14,6 +14,16 @@ class Terminal(Node):
 
 		return True
 
+	@property
+	@override
+	def can_expand(self) -> bool:
+		"""
+		Note:
+			FIXME 比較演算子のみ暗黙的な展開を抑制 ※それ以外の演算子とASTの展開方式が異なるため
+			FIXME 比較演算子だけ抑制すれば良いとは限らないのでTerminalとExpressionの分離を検討
+		"""
+		return self.tag not in ['comp_op', 'comp_in', 'comp_not_in', 'comp_is_not']
+
 
 @Meta.embed(Node, accept_tags('__empty__', 'const_none'))
 class Empty(Node, IDomain, ITerminal):
@@ -21,8 +31,3 @@ class Empty(Node, IDomain, ITerminal):
 	@override
 	def domain_name(self) -> str:
 		return 'Empty'
-
-	@property
-	@implements
-	def can_expand(self) -> bool:
-		return False

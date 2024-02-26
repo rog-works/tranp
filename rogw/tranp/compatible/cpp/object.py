a632bfbbdae45dbf8730edaa1d9e08684b966d55
rog-works <rogangelf@gmail.com>
2024-02-26 09:01:49 +0900

    fix: #160 CVarでC++型変数のタイプ表現が可能な形式に修正

--- rogw/tranp/compatible/cpp/object.py
+++ rogw/tranp/compatible/cpp/object.py
@@ -1,24 +1,79 @@
-from typing import TypeVar
+from abc import ABCMeta, abstractclassmethod
+from typing import Generic, TypeVar
+
+from rogw.tranp.lang.implementation import implements
+
+T = TypeVar('T')
 
 
-class CVar: ...
-class CP(CVar): ...
-class CSP(CVar): ...
-class CRef(CVar): ...
-class CRaw(CVar): ...
+class CVar(Generic[T], metaclass=ABCMeta):
+	@abstractclassmethod
+	def __class_getitem__(cls, var_type: type[T]) -> type[CVar[T]]:
+		raise NotImplementedError()
+
+	def __init__(self, origin: T) -> None:
+		self.__origin = origin
+
+	@property
+	def raw(self) -> T:
+		return self.__origin
+
+	@property
+	def ref(self) -> T:
+		return self.__origin
 
 
-class CMutate: ...
-class CConst(CMutate): ...
-class CMutable(CMutate): ...
-
-
-T_V= TypeVar('T_V', bound=CVar)
-T_M = TypeVar('T_M', bound=CMutate)
-T_Self = TypeVar('T_Self', bound='CObject')
-
-
-class CObject:
+class CP(CVar[T]):
 	@classmethod
-	def __class_getitem__(cls: type[T_Self], var: type[T_V], mutate: type[T_M] = CMutable) -> type[T_Self]:
-		return cls
+	@implements
+	def __class_getitem__(cls, var_type: type[T]) -> type[CP[T]]:
+		return CP[var_type]
+
+
+class CSP(CVar[T]):
+	@classmethod
+	@implements
+	def __class_getitem__(cls, var_type: type[T]) -> type[CSP[T]]:
+		return CSP[var_type]
+
+
+class CRef(CVar[T]):
+	@classmethod
+	@implements
+	def __class_getitem__(cls, var_type: type[T]) -> type[CRef[T]]:
+		return CRef[var_type]
+
+
+class CRaw(CVar[T]):
+	@classmethod
+	@implements
+	def __class_getitem__(cls, var_type: type[T]) -> type[CRaw[T]]:
+		return CRaw[var_type]
+
+
+class CP_Const(CVar[T]):
+	@classmethod
+	@implements
+	def __class_getitem__(cls, var_type: type[T]) -> type[CP_Const[T]]:
+		return CP_Const[var_type]
+
+
+class CSP_Const(CVar[T]):
+	@classmethod
+	@implements
+	def __class_getitem__(cls, var_type: type[T]) -> type[CSP_Const[T]]:
+		return CSP_Const[var_type]
+
+
+class CRef_Const(CVar[T]):
+	@classmethod
+	@implements
+	def __class_getitem__(cls, var_type: type[T]) -> type[CRef_Const[T]]:
+		return CRef_Const[var_type]
+
+
+class CRaw_Const(CVar[T]):
+	@classmethod
+	@implements
+	def __class_getitem__(cls, var_type: type[T]) -> type[CRaw_Const[T]]:
+		return CRaw_Const[var_type]

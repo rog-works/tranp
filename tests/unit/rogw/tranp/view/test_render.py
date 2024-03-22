9810d85d5b5af18599b8a4b067e1af151122155a
rog-works <rogangelf@gmail.com>
2024-03-22 17:34:04 +0900

    test: test_renderの未実装項目を追加(A ~ D)

--- tests/unit/rogw/tranp/view/test_render.py
+++ tests/unit/rogw/tranp/view/test_render.py
@@ -3,6 +3,7 @@ from typing import Any
 from unittest import TestCase
 
 from rogw.tranp.app.dir import tranp_dir
+from rogw.tranp.dsn.translation import to_classes_alias, to_cpp_alias
 from rogw.tranp.test.helper import data_provider
 from rogw.tranp.view.render import Renderer
 
@@ -10,7 +11,19 @@ from rogw.tranp.view.render import Renderer
 class Fixture:
 	@classmethod
 	def renderer(cls) -> Renderer:
-		return Renderer(os.path.join(tranp_dir(), 'data/cpp/template'), lambda key: key)
+		trans_mapping = {
+			to_classes_alias('dict.in'): 'contains',
+			to_classes_alias('dict.append'): 'emplace',
+			to_classes_alias('dict'): 'std::map',
+			to_classes_alias('list'): 'std::vector',
+			to_classes_alias('str'): 'std::string',
+			to_cpp_alias('vector.begin'): 'begin',
+			to_cpp_alias('vector.end'): 'end',
+		}
+		def translator(key: str) -> str:
+			return trans_mapping.get(key, key)
+
+		return Renderer(os.path.join(tranp_dir(), 'data/cpp/template'), translator)
 
 
 class TestRenderer(TestCase):
@@ -27,18 +40,10 @@ class TestRenderer(TestCase):
 		self.assertRender('move_assign', indent, vars, expected)
 
 	@data_provider([
-		({'values': ['1234', '2345']},'{\n\t{1234},\n\t{2345},\n}'),
-		({'values': []}, '{}'),
+		({'symbol': 'B', 'actual_type': 'A'}, 'using B = A;'),
 	])
-	def test_render_list(self, vars: dict[str, Any], expected: str) -> None:
-		self.assertRender('list', 0, vars, expected)
-
-	@data_provider([
-		({'items': ['{hoge, 1}','{fuga, 2}']}, '{\n\t{hoge, 1},\n\t{fuga, 2},\n}'),
-		({'items': []}, '{}'),
-	])
-	def test_render_dict(self, vars: dict[str, Any], expected: str) -> None:
-		self.assertRender('dict', 0, vars, expected)
+	def test_render_alt_class(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('alt_class', 0, vars, expected)
 
 	@data_provider([
 		('move_assign', {'receiver': 'hoge', 'value': '1234'}, 'hoge = 1234;'),
@@ -49,6 +54,29 @@ class TestRenderer(TestCase):
 	])
 	def test_render_assign(self, template: str, vars: dict[str, Any], expected: str) -> None:
 		self.assertRender(template, 0, vars, expected)
+
+	@data_provider([
+		({'value_type': 'float', 'size': '10', 'default': '100.0'}, 'std::vector<float>(10, 100.0)'),
+	])
+	def test_render_binary_operator_fill_list(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('binary_operator_fill_list', 0, vars, expected)
+
+	@data_provider([
+		({'left': 'key', 'operator': 'in', 'right': 'items', 'right_is_dict': True}, 'items.contains(key)'),
+		({'left': 'key', 'operator': 'not.in', 'right': 'items', 'right_is_dict': True}, '(!items.contains(key))'),
+		({'left': 'value', 'operator': 'in', 'right': 'values', 'right_is_dict': False}, '(std::find(values.begin(), values.end(), value) != values.end())'),
+		({'left': 'value', 'operator': 'not.in', 'right': 'values', 'right_is_dict': False}, '(std::find(values.begin(), values.end(), value) == values.end())'),
+		({'left': 'a', 'operator': 'is', 'right': 'b', 'right_is_dict': False}, 'a == b'),
+		({'left': 'a', 'operator': 'is.not', 'right': 'b', 'right_is_dict': False}, 'a != b'),
+		({'left': 'a', 'operator': 'and', 'right': 'b', 'right_is_dict': False}, 'a && b'),
+		({'left': 'a', 'operator': 'or', 'right': 'b', 'right_is_dict': False}, 'a || b'),
+		({'left': 'a', 'operator': '+', 'right': 'b', 'right_is_dict': False}, 'a + b'),
+		({'left': 'a', 'operator': '-', 'right': 'b', 'right_is_dict': False}, 'a - b'),
+		({'left': 'a', 'operator': '*', 'right': 'b', 'right_is_dict': False}, 'a * b'),
+		({'left': 'a', 'operator': '/', 'right': 'b', 'right_is_dict': False}, 'a / b'),
+	])
+	def test_render_binary_operator(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('binary_operator', 0, vars, expected)
 
 	@data_provider([
 		(
@@ -68,6 +96,170 @@ class TestRenderer(TestCase):
 	])
 	def test_render_block(self, vars: dict[str, Any], expected: str) -> None:
 		self.assertRender('block', 0, vars, expected)
+
+	@data_provider([
+		({'var_type': 'Exception', 'symbol': 'e', 'block': 'pass'}, 'catch (Exception e) {\n\tpass\n}'),
+	])
+	def test_render_catch(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('catch', 0, vars, expected)
+
+	@data_provider([
+		({'access': 'public', 'decl_class_var': 'float a'}, 'public: static float a'),
+	])
+	def test_render_class_decl_class_var(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('class_decl_class_var', 0, vars, expected)
+
+	@data_provider([
+		({'access': 'public', 'var_type': 'float', 'symbol': 'a'}, 'public: float a;'),
+	])
+	def test_render_class_decl_this_var(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('class_decl_this_var', 0, vars, expected)
+
+	@data_provider([
+		({'symbols': ['value'], 'iterates': 'values'}, 'auto& value : values'),
+		({'symbols': ['key', 'value'], 'iterates': 'items'}, 'auto& [key, value] : items'),
+	])
+	def test_render_comp_for(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('comp_for', 0, vars, expected)
+
+	@data_provider([
+		({'initializers': [], 'super_initializer': {}}, ''),
+		({'initializers': [{'symbol': 'a', 'value': '1'}, {'symbol': 'b', 'value': '2'}], 'super_initializer': {}}, ' : a(1), b(2)'),
+		({'initializers': [], 'super_initializer': {'parent': 'A', 'arguments': 'a, b'}}, ' : A(a, b)'),
+		({'initializers': [{'symbol': 'a', 'value': '1'}], 'super_initializer': {'parent': 'A', 'arguments': 'a, b'}}, ' : A(a, b), a(1)'),
+	])
+	def test_render_constructor_initializer(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('constructor_initializer', 0, vars, expected)
+
+	@data_provider([
+		({'path': 'classmethod', 'arguments': []}, ''),
+		({'path': 'abstractmethod', 'arguments': []}, ''),
+		({'path': '__alias__', 'arguments': []}, ''),
+		({'path': '__allow_override__', 'arguments': []}, ''),
+		({'path': 'deco', 'arguments': ['a', 'b']}, 'deco(a, b)'),
+	])
+	def test_render_decorator(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('decorator', 0, vars, expected)
+
+	@data_provider([
+		(
+			{
+				'projection': '{key, {value}}',
+				'comp_for': 'auto& [key, value] : items',
+				'condition': '',
+				'projection_types': ['int', 'float'],
+				'binded_this': False,
+			},
+			'\n'.join([
+				'[&]() -> std::map<int, float> {',
+				'	std::map<int, float> __ret;',
+				'	for (auto& [key, value] : items) {',
+				'		__ret.emplace({key, {value}});',
+				'	}',
+				'	return __ret;',
+				'}()',
+			]),
+		),
+		(
+			{
+				'projection': '{key, {value}}',
+				'comp_for': 'auto& [key, value] : items',
+				'condition': 'key == 1',
+				'projection_types': ['int', 'float'],
+				'binded_this': True,
+			},
+			'\n'.join([
+				'[this, &]() -> std::map<int, float> {',
+				'	std::map<int, float> __ret;',
+				'	for (auto& [key, value] : items) {',
+				'		if (key == 1) {',
+				'			__ret.emplace({key, {value}});',
+				'		}',
+				'	}',
+				'	return __ret;',
+				'}()',
+			]),
+		),
+	])
+	def test_render_dict_comp(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('dict_comp', 0, vars, expected)
+
+	@data_provider([
+		({'key_type': 'int', 'value_type': 'float'}, 'std::map<int, float>'),
+	])
+	def test_render_dict_type(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('dict_type', 0, vars, expected)
+
+	@data_provider([
+		({'items': ['{hoge, 1}','{fuga, 2}']}, '{\n\t{hoge, 1},\n\t{fuga, 2},\n}'),
+		({'items': []}, '{}'),
+	])
+	def test_render_dict(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('dict', 0, vars, expected)
+
+	@data_provider([
+		(
+			{
+				'data': {
+					'description': 'Description',
+					'attributes': [],
+					'args': [
+						{'name': 'a', 'type': 'A', 'description': 'A desc'},
+						{'name': 'b', 'type': 'B', 'description': 'B desc'},
+					],
+					'returns': {'type': 'R', 'description': 'R desc'},
+					'raises': [
+						{'type': 'E1', 'description': 'E1 desc'},
+						{'type': 'E2', 'description': 'E2 desc'},
+					],
+					'note': 'Some note',
+					'examples': 'Some example',
+				},
+			},
+			'\n'.join([
+				'/**',
+				' * Description',
+				' * @param a A desc',
+				' * @param b B desc',
+				' * @return R desc',
+				' * @throw E1 E1 desc',
+				' * @throw E2 E2 desc',
+				' * @note Some note',
+				' * @example Some example',
+				' */',
+			]),
+		),
+		(
+			{
+				'data': {
+					'description': 'Description',
+					'attributes': [
+						{'name': 'a', 'type': 'A', 'description': 'A desc'},
+						{'name': 'b', 'type': 'B', 'description': 'B desc'},
+					],
+					'args': [],
+					'returns': {'type': '', 'description': ''},
+					'raises': [],
+					'note': '',
+					'examples': '',
+				},
+			},
+			'\n'.join([
+				'/**',
+				' * Description',
+				' */',
+			]),
+		),
+	])
+	def test_render_doc_string(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('doc_string', 0, vars, expected)
+
+	@data_provider([
+		({'values': ['1234', '2345']},'{\n\t{1234},\n\t{2345},\n}'),
+		({'values': []}, '{}'),
+	])
+	def test_render_list(self, vars: dict[str, Any], expected: str) -> None:
+		self.assertRender('list', 0, vars, expected)
 
 	@data_provider([
 		({'calls': 'A.func', 'arguments': ['1 + 2', 'A.value']}, 'A.func(1 + 2, A.value)'),
@@ -409,60 +601,3 @@ class TestRenderer(TestCase):
 	])
 	def test_render_function(self, template: str, vars: dict[str, Any], expected: str) -> None:
 		self.assertRender(template, 0, vars, expected)
-
-	@data_provider([
-		(
-			{
-				'data': {
-					'description': 'Description',
-					'attributes': [],
-					'args': [
-						{'name': 'a', 'type': 'A', 'description': 'A desc'},
-						{'name': 'b', 'type': 'B', 'description': 'B desc'},
-					],
-					'returns': {'type': 'R', 'description': 'R desc'},
-					'raises': [
-						{'type': 'E1', 'description': 'E1 desc'},
-						{'type': 'E2', 'description': 'E2 desc'},
-					],
-					'note': 'Some note',
-					'examples': 'Some example',
-				},
-			},
-			'\n'.join([
-				'/**',
-				' * Description',
-				' * @param a A desc',
-				' * @param b B desc',
-				' * @return R desc',
-				' * @throw E1 E1 desc',
-				' * @throw E2 E2 desc',
-				' * @note Some note',
-				' * @example Some example',
-				' */',
-			]),
-		),
-		(
-			{
-				'data': {
-					'description': 'Description',
-					'attributes': [
-						{'name': 'a', 'type': 'A', 'description': 'A desc'},
-						{'name': 'b', 'type': 'B', 'description': 'B desc'},
-					],
-					'args': [],
-					'returns': {'type': '', 'description': ''},
-					'raises': [],
-					'note': '',
-					'examples': '',
-				},
-			},
-			'\n'.join([
-				'/**',
-				' * Description',
-				' */',
-			]),
-		),
-	])
-	def test_render_doc_string(self, vars: dict[str, Any], expected: str) -> None:
-		self.assertRender('doc_string', 0, vars, expected)

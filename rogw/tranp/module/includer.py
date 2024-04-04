a6fe55e2944c76860c02afeadada54e07de24148
rog-works <rogangelf@gmail.com>
2024-04-04 11:00:21 +0900

    fix: includerの修正

--- rogw/tranp/module/includer.py
+++ rogw/tranp/module/includer.py
@@ -20,11 +20,12 @@ def include_module_paths(input_glob: str, exclude_patterns: list[str]) -> Module
 	candidate_filepaths = glob.glob(input_glob, recursive=True)
 	exclude_exps = [re.compile(rf'{exclude.replace("*", '.*')}') for exclude in exclude_patterns]
 	module_paths = ModulePaths()
-	for filepath in candidate_filepaths:
+	for org_filepath in candidate_filepaths:
+		filepath = org_filepath.replace(os.path.sep, '/')
 		excluded = len([True for exclude_exp in exclude_exps if exclude_exp.fullmatch(filepath)]) > 0
 		if not excluded:
 			basepath, extention = os.path.splitext(filepath)
-			module_paths.append(ModulePath(basepath.replace(os.path.sep, '.'), language=extention[1:]))
+			module_paths.append(ModulePath(basepath.replace('/', '.'), language=extention[1:]))
 
 	if len(module_paths) == 0:
 		raise LogicError(f'No target found. input_dir: {input_glob}, excludes: {exclude_patterns}')

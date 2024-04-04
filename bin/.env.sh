92ced40fa1fde48f4d1067f03337bc0120a02321
rog-works <rogangelf@gmail.com>
2024-04-04 10:27:39 +0900

    fix: cwdの不備を修正

--- bin/.env.sh
+++ bin/.env.sh
@@ -4,4 +4,4 @@ cwd=$(cd $(dirname $0); pwd)
 appdir=${cwd}/..
 
 export PYTHONDONTWRITEBYTECODE=1
-export PYTHONPATH=${pwd}:${appdir}:${appdir}/vendor
+export PYTHONPATH=$(pwd):${appdir}:${appdir}/vendor

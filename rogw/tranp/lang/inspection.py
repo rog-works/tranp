0d06a6335ec105385eca820b1c2a7b740002a0fc
rog-works <rogangelf@gmail.com>
2024-07-14 16:58:00 +0900

    feat: Annotationを改善

--- rogw/tranp/lang/inspection.py
+++ rogw/tranp/lang/inspection.py
@@ -1,39 +1,283 @@
-from typing import Callable
+from enum import Enum, EnumType
+from types import FunctionType, MethodType, NoneType, UnionType
+from typing import Any, Callable
+
+from rogw.tranp.lang.annotation import override
+from rogw.tranp.lang.comment import Comment
 
 
-class ValueAnnotation:
-	def __init__(self, origin: type) -> None:
-		self.__type = origin
-
-	@property
-	def org_type(self) -> type:
-		return self.__type
-
-	@property
-	def is_generic(self) -> bool:
-		return hasattr(self.__type, '__origin__')
-
-	@property
-	def is_list(self) -> bool:
-		return self.is_generic and self.origin is list
+class Annotation:
+	"""アノテーション(基底クラス)"""
 
 	@property
 	def origin(self) -> type:
-		return getattr(self.__type, '__origin__')
+		"""type: メインタイプ"""
+		...
 
 
-class FunctionAnnotation:
-	def __init__(self, func: Callable) -> None:
-		self.__func = func
+class ScalarAnnotation(Annotation):
+	"""アノテーション(値)
+
+	Note:
+		### 対象のクラス
+		* int/str/float/bool
+		* list/dict/tuple/type
+		* UnionType
+		* EnumType
+		* None/NoneType
+	"""
+
+	def __init__(self, scalar_type: type) -> None:
+		"""インスタンスを生成
+
+		Args:
+			scalar_type (type): タイプ
+		"""
+		self._type = scalar_type
 
 	@property
-	def args(self) -> dict[str, ValueAnnotation]:
-		return {key: ValueAnnotation(in_type) for key, in_type in self.__annos.items() if key != 'return'}
+	@override
+	def origin(self) -> type:
+		"""type: メインタイプ"""
+		return getattr(self._type, '__origin__', self._type)
 
 	@property
-	def return_type(self) -> ValueAnnotation:
-		return ValueAnnotation(self.__annos['return'])
+	def raw(self) -> type:
+		"""type: 元のタイプ"""
+		return self._type
+
+	@property
+	def is_null(self) -> bool:
+		"""bool: True = None"""
+		return self._type is None or self._type is NoneType
+
+	@property
+	def is_generic(self) -> bool:
+		"""bool: True = ジェネリック型"""
+		return hasattr(self._type, '__origin__') or self.origin in [list, dict, tuple, type]
+
+	@property
+	def is_list(self) -> bool:
+		"""bool: True = リスト型"""
+		return self.origin is list
+
+	@property
+	def is_union(self) -> bool:
+		"""bool: True = Union型"""
+		return type(self._type) is UnionType
+
+	@property
+	def is_nullable(self) -> bool:
+		"""bool: True = Nullable型"""
+		return self.is_union and type(None) in self.__sub_annos
+
+	@property
+	def is_enum(self) -> bool:
+		"""bool: True = Enum型"""
+		return type(self._type) is EnumType
+
+	@property
+	def sub_types(self) -> list[Annotation]:
+		"""list[Annotation]: ジェネリック/Union型のサブタイプのリスト"""
+		return [AnnotationResolver.resolve(sub_type) for sub_type in self.__sub_annos]
+
+	@property
+	def __sub_annos(self) -> list[type]:
+		"""list[type]: ジェネリック/Union型のサブタイプのリスト"""
+		return getattr(self._type, '__args__', [])
+
+	@property
+	def enum_members(self) -> list[Enum]:
+		"""dict[str, Enum]: Enumのメンバー一覧"""
+		return list(getattr(self.origin, '__members__').values())
+
+
+class FunctionAnnotation(Annotation):
+	"""アノテーション(関数)
+
+	Note:
+		### 対象のクラス
+		* 関数
+		* メソッド(クラス/インスタンス)
+		### 非対応
+		* ラムダ ※アノテーションが付けられないため
+	"""
+
+	def __init__(self, func_or_class: Callable) -> None:
+		"""インスタンスを生成
+
+		Args:
+			func_or_class (Callable): 関数・メソッド・クラス
+		Note:
+			クラスを指定した場合は暗黙的にコンストラクターを展開する
+		"""
+		self._func = func_or_class if isinstance(func_or_class, (FunctionType, MethodType)) else func_or_class.__init__
+
+	@property
+	@override
+	def origin(self) -> type:
+		"""type: メインタイプ"""
+		return type(self._func)
+
+	@property
+	def raw(self) -> Callable:
+		"""Callable: 元の型"""
+		return self._func
+
+	@property
+	def is_static(self) -> bool:
+		"""bool: True = クラスメソッド"""
+		return isinstance(getattr(self._func, '__self__', None), type)
+
+	@property
+	def is_method(self) -> bool:
+		"""bool: True = メソッド"""
+		return isinstance(self._func, MethodType)
+
+	@property
+	def is_function(self) -> bool:
+		"""bool: True = 関数"""
+		return isinstance(self._func, FunctionType)
+
+	@property
+	def receiver(self) -> Any:
+		"""メソッドレシーバーを返却。取得の成否、及び取得対象は生成元に依存する
+
+		Returns:
+			Any: メソッドレシーバー
+		Note:
+			### クラスから生成した場合
+			* クラスメソッド: クラス
+			* インスタンスメソッド: NG
+			### インスタンスから生成した場合
+			* クラスメソッド: クラス
+			* インスタンスメソッド: インスタンス
+		Raises:
+			TypeError: 関数、または取得できない条件で使用
+		"""
+		return getattr(self._func, '__self__')
+
+	@property
+	def args(self) -> dict[str, Annotation]:
+		"""dict[str, Annotation]: 引数リスト"""
+		return {key: AnnotationResolver.resolve(in_type) for key, in_type in self.__annos.items() if key != 'return'}
+
+	@property
+	def returns(self) -> Annotation:
+		"""Annotation: 戻り値"""
+		return AnnotationResolver.resolve(self.__annos['return'])
 
 	@property
 	def __annos(self) -> dict[str, type]:
-		return self.__func.__annotations__
+		"""dict[str, type]: アノテーションのリスト"""
+		return self._func.__annotations__
+
+
+class ClassAnnotation(Annotation):
+	"""アノテーション(クラス)
+
+	Note:
+		### 対象のクラス
+		* クラス全般
+		### 注意事項
+		* @see class_vars クラス変数の展開
+		* @see self_vars インスタンス変数の展開
+	"""
+
+	def __init__(self, class_type: type) -> None:
+		"""インスタンスを生成
+
+		Args:
+			class_type (type): クラス
+		"""
+		self._type = class_type
+
+	@property
+	@override
+	def origin(self) -> type:
+		"""type: メインタイプ"""
+		return self._type
+
+	@property
+	def constructor(self) -> FunctionAnnotation:
+		"""FunctionAnnotation: コンストラクター"""
+		return FunctionAnnotation(self._type.__init__)
+
+	@property
+	def class_vars(self) -> dict[str, Annotation]:
+		"""クラス変数の一覧を取得
+
+		Returns:
+			dict[str, Annotation]: クラス変数一覧
+		Note:
+			Pythonではクラス変数とインスタンス変数の差が厳密ではないため、
+			このクラスではクラス直下に定義された変数をクラス変数と位置づける
+		"""
+		annos = {key: AnnotationResolver.resolve(prop) for key, prop in self._type.__annotations__.items()}
+		return {key: prop for key, prop in annos.items() if prop is not FunctionAnnotation}
+
+	@property
+	def self_vars(self) -> dict[str, Annotation]:
+		"""インスタンス変数の一覧を取得
+
+		Returns:
+			dict[str, Annotation]: インスタンス変数一覧
+		Note:
+			Pythonでは、クラスからインスタンス変数を確実に抜き出す方法が無いため、
+			クラスに記入されたDocStringを基にインスタンス変数のアノテーションを生成する
+		"""
+		docstring = Comment.parse(getattr(self._type, '__doc__', ''))
+		var_types: dict[str, type] = {attr.name: eval(attr.type) for attr in docstring.attributes}
+		return {key: AnnotationResolver.resolve(prop) for key, prop in var_types.items()}
+
+	@property
+	def methods(self) -> dict[str, FunctionAnnotation]:
+		"""dict[str, FunctionAnnotation]: メソッド一覧"""
+		# XXX __dict__の値とgetattrで取得する値に相違があるため、getattrを使う
+		_methods: dict[str, FunctionType | MethodType] = {}
+		for key in self._type.__dict__.keys():
+			attr = getattr(self._type, key)
+			if isinstance(attr, (FunctionType, MethodType)):
+				_methods[key] = attr
+
+		return {key: FunctionAnnotation(prop) for key, prop in _methods.items()}
+
+
+class AnnotationResolver:
+	"""アノテーションリゾルバー"""
+
+	@classmethod
+	def resolve(cls, origin: type | FunctionType | MethodType) -> Annotation:
+		"""アノテーションを解決
+
+		Args:
+			origin (type | FunctionType | MethodType): タイプ
+		Returns:
+			Annotation: アノテーション
+		"""
+		if isinstance(origin, (FunctionType, MethodType)):
+			return FunctionAnnotation(origin)
+		elif cls.__is_scalar(origin):
+			return ScalarAnnotation(origin)
+		else:
+			return ClassAnnotation(origin)
+
+	@classmethod
+	def __is_scalar(cls, origin: type) -> bool:
+		"""値型か判定
+
+		Args:
+			origin (type): タイプ
+		Returns:
+			bool: True = 値型
+		"""
+		if isinstance(origin, type) and issubclass(origin, (int, str, float, bool)):
+			return True
+		elif getattr(origin, '__origin__', origin) in [list, dict, tuple, type]:
+			return True
+		elif type(origin) in [UnionType, EnumType]:
+			return True
+		elif origin is None or origin is NoneType:
+			return True
+		else:
+			return False
---
+++ tests/unit/rogw/tranp/lang/test_inspection.py
@@ -0,0 +1,194 @@
+from enum import Enum, EnumType
+from types import NoneType
+from typing import Any, Callable, cast
+from unittest import TestCase
+
+from rogw.tranp.lang.inspection import Annotation, AnnotationResolver, ClassAnnotation, FunctionAnnotation, ScalarAnnotation
+from rogw.tranp.test.helper import data_provider
+
+
+class TestScalarAnnotation(TestCase):
+	class Values(Enum):
+		A = 1
+
+	@data_provider([
+		(int, {'raw': int, 'origin': int}),
+		(list, {'raw': list, 'origin': list}),
+		(list[int], {'raw': list[int], 'origin': list}),
+		(dict, {'raw': dict, 'origin': dict}),
+		(dict[str, int], {'raw': dict[str, int], 'origin': dict}),
+		(tuple, {'raw': tuple, 'origin': tuple}),
+		(tuple[str, int, bool], {'raw': tuple[str, int, bool], 'origin': tuple}),
+		(type, {'raw': type, 'origin': type}),
+		(type[str], {'raw': type[str], 'origin': type}),
+		(int | str, {'raw': int | str, 'origin': int | str}),  # XXX origin=UnionTypeの方が良い？
+		(int | None, {'raw': int | None, 'origin': int | None}),  # XXX origin=UnionTypeの方が良い？
+		(None, {'raw': None, 'origin': None}),
+		(Values, {'raw': Values, 'origin': Values}),
+	])
+	def test_origins(self, origin: type, expected: dict[str, type]) -> None:
+		anno = ScalarAnnotation(origin)
+		self.assertEqual(expected['raw'], anno.raw)
+		self.assertEqual(expected['origin'], anno.origin)
+
+	@data_provider([
+		(int, {'null': False, 'generic': False, 'list': False, 'union': False, 'nullable': False, 'enum': False}),
+		(list, {'null': False, 'generic': True, 'list': True, 'union': False, 'nullable': False, 'enum': False}),
+		(list[int], {'null': False, 'generic': True, 'list': True, 'union': False, 'nullable': False, 'enum': False}),
+		(dict, {'null': False, 'generic': True, 'list': False, 'union': False, 'nullable': False, 'enum': False}),
+		(dict[str, int], {'null': False, 'generic': True, 'list': False, 'union': False, 'nullable': False, 'enum': False}),
+		(tuple, {'null': False, 'generic': True, 'list': False, 'union': False, 'nullable': False, 'enum': False}),
+		(tuple[str, int, bool], {'null': False, 'generic': True, 'list': False, 'union': False, 'nullable': False, 'enum': False}),
+		(type, {'null': False, 'generic': True, 'list': False, 'union': False, 'nullable': False, 'enum': False}),
+		(type[str], {'null': False, 'generic': True, 'list': False, 'union': False, 'nullable': False, 'enum': False}),
+		(int | str, {'null': False, 'generic': False, 'list': False, 'union': True, 'nullable': False, 'enum': False}),
+		(int | None, {'null': False, 'generic': False, 'list': False, 'union': True, 'nullable': True, 'enum': False}),
+		(None, {'null': True, 'generic': False, 'list': False, 'union': False, 'nullable': False, 'enum': False}),
+		(Values, {'null': False, 'generic': False, 'list': False, 'union': False, 'nullable': False, 'enum': True}),
+	])
+	def test_states(self, origin: type, expected: dict[str, bool]) -> None:
+		anno = ScalarAnnotation(origin)
+		self.assertEqual(expected['null'], anno.is_null)
+		self.assertEqual(expected['generic'], anno.is_generic)
+		self.assertEqual(expected['list'], anno.is_list)
+		self.assertEqual(expected['union'], anno.is_union)
+		self.assertEqual(expected['nullable'], anno.is_nullable)
+		self.assertEqual(expected['enum'], anno.is_enum)
+
+	@data_provider([
+		(int, []),
+		(list, []),
+		(list[int], [int]),
+		(dict, []),
+		(dict[str, int], [str, int]),
+		(tuple, []),
+		(tuple[str, int, bool], [str, int, bool]),
+		(type, []),
+		(type[str], [str]),
+		(int | str, [int, str]),
+		(int | None, [int, NoneType]),
+		(Values, []),
+	])
+	def test_sub_types(self, origin: type, expected: list[type]) -> None:
+		self.assertEqual(expected, [cast(ScalarAnnotation, anno).origin for anno in ScalarAnnotation(origin).sub_types])
+
+	@data_provider([
+		(Values, [Values.A]),
+	])
+	def test_enum_members(self, origin: EnumType, expected: list[Enum]) -> None:
+		self.assertEqual(expected, ScalarAnnotation(origin).enum_members)
+
+
+def func(n: int) -> str: ...
+
+
+class TestFunctionAnnotation(TestCase):
+	class Test:
+		def __init__(self) -> None: ...
+		@classmethod
+		def cls_func(cls, n: int) -> str: ...
+		def self_func(self, l: list[int], d: dict[str, int]) -> tuple[int, str, bool]: ...
+
+	@data_provider([
+		(Test.__init__, {'static': False, 'method': False, 'function': True}),  # FIXME 静的に取得するとFunctionTypeになり、メソッドであるか否かを判別できない
+		(Test.cls_func, {'static': True, 'method': True, 'function': False}),
+		(Test.self_func, {'static': False, 'method': False, 'function': True}),  # FIXME 静的に取得するとFunctionTypeになり、メソッドであるか否かを判別できない
+		(Test().cls_func, {'static': True, 'method': True, 'function': False}),
+		(Test().self_func, {'static': False, 'method': True, 'function': False}),
+		(func, {'static': False, 'method': False, 'function': True}),
+	])
+	def test_states(self, origin: Callable, expected: dict[str, bool]) -> None:
+		anno = FunctionAnnotation(origin)
+		self.assertEqual(expected['static'], anno.is_static)
+		self.assertEqual(expected['method'], anno.is_method)
+		self.assertEqual(expected['function'], anno.is_function)
+
+	@data_provider([
+		(Test.cls_func, Test),
+		(Test().cls_func, Test),
+		(Test().self_func, Test),
+	])
+	def test_receiver(self, origin: Callable, expected: type) -> None:
+		anno = FunctionAnnotation(origin)
+		self.assertEqual(expected, anno.receiver if anno.is_static else anno.receiver.__class__)
+
+	@data_provider([
+		(Test.__init__,),  # FIXME 静的に取得するとreceiverを取得できない
+		(Test.self_func,),  # FIXME 静的に取得するとreceiverを取得できない
+		(func,),
+	])
+	def test_receiver_error(self, origin: Callable) -> None:
+		with self.assertRaises(AttributeError):
+			FunctionAnnotation(origin).receiver
+
+	@data_provider([
+		(Test.__init__, {'args': {}, 'returns': None}),
+		(Test.cls_func, {'args': {'n': int}, 'returns': str}),
+		(Test.self_func, {'args': {'l': list, 'd': dict}, 'returns': tuple}),
+		(Test().cls_func, {'args': {'n': int}, 'returns': str}),
+		(Test().self_func, {'args': {'l': list, 'd': dict}, 'returns': tuple}),
+		(func, {'args': {'n': int}, 'returns': str}),
+	])
+	def test_signature(self, origin: Callable, expected: dict[str, Any]) -> None:
+		anno = FunctionAnnotation(origin)
+		self.assertEqual(expected['args'], {key: arg.origin for key, arg in anno.args.items()})
+		self.assertEqual(expected['returns'], anno.returns.origin)
+
+
+class TestClassAnnotation(TestCase):
+	class Test:
+		"""DocStringによってインスタンス変数を明示する
+
+		Attributes:
+			d (dict[str, int]): dict
+		"""
+
+		n: int = 0
+		l: list[int] = []
+
+		def __init__(self) -> None:
+			self.d: dict[str, int] = {}
+
+		@classmethod
+		def cls_method(cls) -> None: ...
+		def self_method(self) -> None: ...
+
+	@data_provider([
+		(Test, Test.__init__),
+	])
+	def test_constructor(self, origin: type, expected: Callable) -> None:
+		anno = ClassAnnotation(origin)
+		self.assertEqual(expected, anno.constructor.raw)
+
+	@data_provider([
+		(Test, {'class_vars': {'n': int, 'l': list}, 'self_vars': {'d': dict}, 'methods': ['__init__', 'cls_method', 'self_method']}),
+	])
+	def test_schema(self, origin: type, expected: dict[str, Any]) -> None:
+		anno = ClassAnnotation(origin)
+		self.assertEqual(expected['class_vars'], {key: var.origin for key, var in anno.class_vars.items()})
+		self.assertEqual(expected['self_vars'], {key: var.origin for key, var in anno.self_vars.items()})
+		self.assertEqual(expected['methods'], [key for key in anno.methods.keys()])
+
+
+class TestAnnotationResolver(TestCase):
+	@data_provider([
+		(int, ScalarAnnotation),
+		(str, ScalarAnnotation),
+		(float, ScalarAnnotation),
+		(bool, ScalarAnnotation),
+		(list, ScalarAnnotation),
+		(list[int], ScalarAnnotation),
+		(dict, ScalarAnnotation),
+		(dict[str, int], ScalarAnnotation),
+		(tuple[str, int, bool], ScalarAnnotation),
+		(int | str, ScalarAnnotation),
+		(type, ScalarAnnotation),
+		(type[str], ScalarAnnotation),
+		(None, ScalarAnnotation),
+		(type(None), ScalarAnnotation),
+		(func, FunctionAnnotation),
+		(AnnotationResolver.resolve, FunctionAnnotation),
+		(AnnotationResolver, ClassAnnotation),
+	])
+	def test_resolve(self, origin: type, expected: Annotation) -> None:
+		self.assertEqual(expected, type(AnnotationResolver.resolve(origin)))

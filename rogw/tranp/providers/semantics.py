966e7a8df255b297646cb930e30e7fc4d3846111
rog-works <rogangelf@gmail.com>
2024-08-21 10:15:07 +0900

    fix: ファイルの配置を修正

--- rogw/tranp/providers/semantics.py
+++ rogw/tranp/providers/semantics.py
@@ -1,4 +1,5 @@
 from rogw.tranp.lang.annotation import injectable
+from rogw.tranp.semantics.plugin import PluginProvider
 from rogw.tranp.semantics.processor import Preprocessors
 from rogw.tranp.semantics.reflection.db import SymbolDB, SymbolDBProvider
 
@@ -17,3 +18,12 @@ def make_db(preprocessors: Preprocessors) -> SymbolDBProvider:
 		db = proc(db)
 
 	return SymbolDBProvider(db)
+
+
+def plugin_provider_empty() -> PluginProvider:
+	"""プラグインプロバイダーを生成(空)
+
+	Returns:
+		PluginProvider: プラグインプロバイダー
+	"""
+	return lambda: []

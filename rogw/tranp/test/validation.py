d7c4c952138e7b0ad47abbb57d7a40a8e21935b2
rog-works <rogangelf@gmail.com>
2024-08-29 08:07:56 +0900

    fix: 解析可能にするために微調整&validationを分離

---
+++ rogw/tranp/test/validation.py
@@ -0,0 +1,28 @@
+from typing import Callable, TypeVar
+
+from rogw.tranp.lang.inspection import ClassTypehint
+
+T = TypeVar('T')
+
+
+def validation(klass: type[T], factory: Callable[[], T] | None = None) -> bool:
+	"""クラスの実装スキーマバリデーション
+
+	Args:
+		klass (type[T]): 検証クラス
+		factory (Callable[[], T] | None): インスタンスファクトリー (default = None)
+	Returns:
+		bool: True = 成功
+	Raises:
+		TypeError: 設計と実体の不一致
+	"""
+	hint = ClassTypehint(klass)
+	instance = factory() if factory else klass()
+	hint_keys = hint.self_vars.keys()
+	inst_keys = instance.__dict__.keys()
+	exists_from_hint = [key for key in hint_keys if key in inst_keys]
+	exists_from_inst = [key for key in inst_keys if key in hint_keys]
+	if len(exists_from_hint) != len(exists_from_inst):
+		raise TypeError(f'Schema not match. class: {klass.__name__}, expected self attributes: ({",".join(hint_keys)})')
+
+	return True

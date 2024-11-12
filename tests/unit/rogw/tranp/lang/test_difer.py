758f9892514e85f019befa1fb1d2db7c53c1104e
rog-works <rogangelf@gmail.com>
2024-11-12 14:58:09 +0900

    test: Deferのテストケース追加

--- tests/unit/rogw/tranp/lang/test_difer.py
+++ tests/unit/rogw/tranp/lang/test_difer.py
@@ -1,13 +1,46 @@
+from typing import Any, Callable
 from unittest import TestCase
 
 from rogw.tranp.lang.defer import Difer
+from rogw.tranp.test.helper import data_provider
+
+
+class A:
+	def func(self) -> int:
+		return 1
 
 
 class TestDifer(TestCase):
-	def test_usage(self) -> None:
-		class A:
-			def func(self) -> int:
-				return 1
-
-		actual = Difer.new(lambda: A())
-		self.assertEqual(actual.func(), 1)
+	@data_provider([
+		(lambda: A(), {
+			'class': A,
+			'invoke': (A.func.__name__, 1),
+			'eq': False,
+			'ne': True,
+		}),
+		(lambda: 'abc', {
+			'class': str,
+			'invoke': (str.split.__name__, ['abc']),
+			'str': 'abc',
+			'len': 3,
+			'eq': True,
+			'ne': False,
+			'add': ('d', 'abcd'),
+			'getitem': (slice(1, 3), 'bc'),
+		}),
+	])
+	def test_usage(self, factory: Callable[[], Any], expected: dict[str, Any]) -> None:
+		instance = Difer.new(factory)
+		invoker: Callable[[], Any] = getattr(instance, expected['invoke'][0])
+		self.assertEqual(instance.__class__, expected['class'])
+		self.assertEqual(invoker(), expected['invoke'][1])
+		self.assertEqual(instance == factory(), expected['eq'])
+		self.assertEqual(instance != factory(), expected['ne'])
+		if 'str' in expected:
+			self.assertEqual(str(instance), expected['str'])
+		if 'len' in expected:
+			self.assertEqual(len(instance), expected['len'])
+		if 'add' in expected:
+			self.assertEqual(instance + expected['add'][0], expected['add'][1])
+		if 'getitem' in expected:
+			self.assertEqual(instance[expected['getitem'][0]], expected['getitem'][1])

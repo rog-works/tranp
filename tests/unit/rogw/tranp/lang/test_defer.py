58b56115af496644f6af68fdaf0ccd15a7358afe
rog-works <rogangelf@gmail.com>
2024-11-15 08:03:51 +0900

    fix: Deferのdeprecatedを解除

--- tests/unit/rogw/tranp/lang/test_defer.py
+++ tests/unit/rogw/tranp/lang/test_defer.py
@@ -13,22 +13,7 @@ class A:
 
 class TestDefer(TestCase):
 	@data_provider([
-		(lambda: A(), {
-			'class': A,
-			'invoke': (A.func.__name__, 1),
-			'eq': False,
-			'ne': True,
-		}),
-		(lambda: 'abc', {
-			'class': str,
-			'invoke': (str.split.__name__, ['abc']),
-			'str': 'abc',
-			'len': 3,
-			'eq': True,
-			'ne': False,
-			'add': ('d', 'abcd'),
-			'getitem': (slice(1, 3), 'bc'),
-		}),
+		(lambda: A(), {'class': A, 'invoke': (A.func.__name__, 1), 'eq': False, 'ne': True}),
 	])
 	def test_usage(self, factory: Callable[[], Any], expected: dict[str, Any]) -> None:
 		instance = Defer.new(factory)
@@ -38,11 +23,3 @@ class TestDefer(TestCase):
 		self.assertEqual(invoker(), expected['invoke'][1])
 		self.assertEqual(instance == factory(), expected['eq'])
 		self.assertEqual(instance != factory(), expected['ne'])
-		if 'str' in expected:
-			self.assertEqual(str(instance), expected['str'])
-		if 'len' in expected:
-			self.assertEqual(len(instance), expected['len'])
-		if 'add' in expected:
-			self.assertEqual(instance + expected['add'][0], expected['add'][1])
-		if 'getitem' in expected:
-			self.assertEqual(instance[expected['getitem'][0]], expected['getitem'][1])

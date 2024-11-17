f412b5d3a98bd306edc3836aaf0484352ce73f3e
rog-works <rogangelf@gmail.com>
2024-11-18 08:08:12 +0900

    fix: Helperに分離2

--- tests/unit/rogw/tranp/lang/test_parser.py
+++ tests/unit/rogw/tranp/lang/test_parser.py
@@ -1,7 +1,6 @@
-from typing import Any
 from unittest import TestCase
 
-from rogw.tranp.lang.parser import AltFormatter, BlockParser, DecoratorParser, Kinds
+from rogw.tranp.lang.parser import AltFormatter, BlockParser, Kinds
 from rogw.tranp.test.helper import data_provider
 
 
@@ -89,36 +88,4 @@ class TestParser(TestCase):
 	])
 	def test_break_separator(self, text: str, delimiter: str, expected: list[str]) -> None:
 		actual = BlockParser.break_separator(text, delimiter)
-		self.assertEqual(actual, expected)
-
-
-class TestDecoratorParser(TestCase):
-	@data_provider([
-		('abc()', {'path': 'abc', 'args': {}, 'arg': ''}),
-		('Embed.prop("a")', {'path': 'Embed.prop', 'args': {'0': '"a"'}, 'arg': '"a"'}),
-		('Embed.alias("a", prefix=true)', {'path': 'Embed.alias', 'args': {'0': '"a"', 'prefix': 'true'}, 'arg': '"a"'}),
-	])
-	def test_schema(self, decorator: str, expected: dict[str, Any]) -> None:
-		instance = DecoratorParser(decorator)
-		self.assertEqual(instance.path, expected['path'])
-		self.assertEqual(instance.args, expected['args'])
-		self.assertEqual(instance.arg, expected['arg'])
-
-	@data_provider([
-		('abc()', ['abc'], {}, True),
-		('abc()', ['a', 'b', 'c'], {}, False),
-		('Embed.prop("a")', ['Embed.prop'], {}, True),
-		('Embed.prop("a")', ['Embed.prop'], {'0': '"a"'}, True),
-		('Embed.prop("abc")', ['Embed.prop'], {'0': '"a"'}, False),
-	])
-	def test_any(self, decorator: str, paths: list[str], args: dict[str, str], expected: bool) -> None:
-		actual = DecoratorParser(decorator).any(*paths, **args)
-		self.assertEqual(actual, expected)
-
-	@data_provider([
-		('abc()', r'^abc', True),
-		('abc()', r'^abc\(\)$', True),
-	])
-	def test_match(self, decorator: str, pattern: str, expected: bool) -> None:
-		actual = DecoratorParser(decorator).match(pattern)
 		self.assertEqual(actual, expected)

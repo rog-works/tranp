199603c400a3556d1cb45163805329af0e6b1471
rog-works <rogangelf@gmail.com>
2024-12-14 09:58:19 +0900

    test: 基底クラスに分離

--- tests/unit/rogw/tranp/implements/cpp/transpiler/fixtures/test_py2cpp_expect.py
+++ tests/unit/rogw/tranp/implements/cpp/transpiler/fixtures/test_py2cpp_expect.py
@@ -171,14 +171,15 @@ class DeclOps {
 };"""
 
 	DeclProps = """/** DeclProps */
-class DeclProps {
+class DeclProps : public DeclPropsBase {
 	public: inline static bool cls_b = true;
-	public: int anno_n;
-	public: std::string move_s;
 	public: std::map<std::string, int> move_dsn;
 	public:
 	/** __init__ */
-	DeclProps(int n, const std::string& s) : anno_n(atoi(s)), move_s(std::to_string(n)), move_dsn({{s, n}}) {}
+	DeclProps(int n, const std::string& s) : DeclPropsBase(), move_dsn({{s, n}}) {
+		this->anno_n = atoi(s);
+		this->move_s = std::to_string(n);
+	}
 };"""
 
 	ForTemplateClass_Delegate = \

b8e3db6469111503c265a681fbfa5c9d0a35ad81
rog-works <rogangelf@gmail.com>
2025-01-24 12:08:07 +0900

    feat: Enum.valueの追加

--- rogw/tranp/compatible/libralies/enum.py
+++ rogw/tranp/compatible/libralies/enum.py
@@ -2,10 +2,17 @@ from typing import Any
 
 
 class EnumType: ...
+
+
 class Enum:
-	def __init__(self, value: int) -> None: ...
-	@property
-	def name(self) -> str: ...
+	name: str
+	value: Any
+
+	def __init__(self, value: Any) -> None:
+		self.name = ''
+		# FIXME 全く正しくないが実用上は問題ないため一旦これで良しとする
+		self.value = value
+
 	# comparison
 	def __eq__(self, other: Any) -> bool: ...
 	def __ne__(self, other: Any) -> bool: ...

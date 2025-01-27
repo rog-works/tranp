da8789db3063cf30ef1ed39341d3a2ef00b85d2e
rog-works <rogangelf@gmail.com>
2025-01-27 10:48:39 +0900

    test: TokenParserのテストケースを追加

--- tests/unit/rogw/tranp/implements/syntax/tranp/test_parser.py
+++ tests/unit/rogw/tranp/implements/syntax/tranp/test_parser.py
@@ -1,11 +1,21 @@
 from typing import Any
 from unittest import TestCase
 
-from rogw.tranp.implements.syntax.tranp.parser import Lexer, rules
+from rogw.tranp.implements.syntax.tranp.parser import SyntaxParser, TokenParser, rules
 from rogw.tranp.test.helper import data_provider
 
 
-class TestParser(TestCase):
+class TestTokenParser(TestCase):
+	@data_provider([
+		('a.b.c', ['a', '.', 'b', '.', 'c']),
+		('?a _b', ['?', 'a', '_b']),
+	])
+	def test_parse(self, source: str, expected: list[str]) -> None:
+		actual = TokenParser.parse(source)
+		self.assertEqual(expected, [token.string for token in actual])
+
+
+class TestSyntaxParser(TestCase):
 	@data_provider([
 		('a.b.c', {
 			'ast': ('entry', [
@@ -23,7 +33,7 @@ class TestParser(TestCase):
 	])
 	def test_parse(self, source: str, expected: dict[str, Any]) -> None:
 		rules_ = rules()
-		actual = Lexer(rules_).parse(source, 'entry')
+		actual = SyntaxParser(rules_).parse(source, 'entry')
 
 		try:
 			self.assertEqual(expected['ast'], actual)

b7078ec8a0c59ae576b5637557e8608e258e1a91
rog-works <rogangelf@gmail.com>
2025-01-27 15:24:29 +0900

    test: Grammarのテストケースを追加

--- tests/unit/rogw/tranp/implements/syntax/tranp/test_parser.py
+++ tests/unit/rogw/tranp/implements/syntax/tranp/test_parser.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
-from rogw.tranp.implements.syntax.tranp.parser import ASTTree, Operators, Pattern, PatternEntry, Patterns, Repeators, SyntaxParser, TokenParser, python_rules
+from rogw.tranp.implements.syntax.tranp.parser import ASTTree, SyntaxParser, TokenParser
+from rogw.tranp.implements.syntax.tranp.rules import grammar_rules, python_rules
 from rogw.tranp.test.helper import data_provider
 
 
@@ -53,10 +54,21 @@ class TestSyntaxParser(TestCase):
 				])
 			]),
 		),
+		(
+			'a := d\n',
+			'grammar',
+			('entry', [
+				('rule', [
+					('symbol', 'a'),
+					('symbol', 'b'),
+				])
+			]),
+		),
 	])
 	def test_parse(self, source: str, lang: str, expected: ASTTree) -> None:
 		rule_provider = {
 			'python': python_rules,
+			'grammar': grammar_rules,
 		}
 		rules = rule_provider[lang]()
 		actual = SyntaxParser(rules).parse(source, 'entry')

a4d14b39365f4553438c7ff8bff872db59a1df87
rog-works <rogangelf@gmail.com>
2025-01-27 09:02:00 +0900

    test: ソース指定に変更

--- tests/unit/rogw/tranp/implements/syntax/tranp/test_parser.py
+++ tests/unit/rogw/tranp/implements/syntax/tranp/test_parser.py
@@ -2,13 +2,13 @@ import json
 from typing import Any
 from unittest import TestCase
 
-from rogw.tranp.implements.syntax.tranp.parser import Parser, rules
+from rogw.tranp.implements.syntax.tranp.parser import Lexer, rules
 from rogw.tranp.test.helper import data_provider
 
 
 class TestParser(TestCase):
 	@data_provider([
-		(['a', '.', 'b', '.', 'c'], {
+		('a.b.c', {
 			'ast': ('relay', [
 				('relay', [
 					('var', [
@@ -20,8 +20,8 @@ class TestParser(TestCase):
 			]),
 		}),
 	])
-	def test_parse(self, tokens: list[str], expected: dict[str, Any]) -> None:
+	def test_parse(self, source: str, expected: dict[str, Any]) -> None:
 		rules_ = rules()
-		actual = Parser(rules_).parse(tokens, 'entry')
+		actual = Lexer(rules_).parse(source, 'entry')
 		print(json.dumps(actual))
 		self.assertEqual(expected['ast'], actual)

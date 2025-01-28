679923090b34aaebf96014bab6d62a4b6dca651c
rog-works <rogangelf@gmail.com>
2025-01-28 10:27:20 +0900

    test: TokenParserのテストケースを追加

--- tests/unit/rogw/tranp/implements/syntax/tranp/test_tokenizer.py
+++ tests/unit/rogw/tranp/implements/syntax/tranp/test_tokenizer.py
@@ -6,6 +6,28 @@ from rogw.tranp.test.helper import data_provider
 
 class TestTokenParser2(TestCase):
 	@data_provider([
+		(' abc', 0, (1, ' ')),
+		('abc ', 3, (4, ' ')),
+		('ab c', 2, (3, ' ')),
+		('a \t\nb\nc', 1, (4, ' \t\n')),
+	])
+	def test_parse_white_space(self, source: str, begin: int, expected: tuple[int, str]) -> None:
+		parser = TokenParser2()
+		actual = parser.parse_white_spece(source, begin)
+		self.assertEqual(expected, actual)
+
+	@data_provider([
+		('a # bc', 2, (6, '# bc')),
+		('a # bc\n', 2, (7, '# bc\n')),
+		('a """bc""" d', 2, (10, '"""bc"""')),
+		('a """b\nc""" d', 2, (11, '"""b\nc"""')),
+	])
+	def test_parse_comment(self, source: str, begin: int, expected: tuple[int, str]) -> None:
+		parser = TokenParser2()
+		actual = parser.parse_comment(source, begin)
+		self.assertEqual(expected, actual)
+
+	@data_provider([
 		('1234', 0, (4, '1234')),
 		('1234.', 0, (5, '1234.')),
 		('1.2', 0, (3, '1.2')),
@@ -13,7 +35,49 @@ class TestTokenParser2(TestCase):
 	])
 	def test_parse_number(self, source: str, begin: int, expected: tuple[int, str]) -> None:
 		parser = TokenParser2()
-		parser.parse_number(source, begin)
+		actual = parser.parse_number(source, begin)
+		self.assertEqual(expected, actual)
+
+	@data_provider([
+		('"abc"', 0, (5, '"abc"')),
+		("'abc'", 0, (5, "'abc'")),
+		('(a./abc/)', 3, (8, '/abc/')),
+	])
+	def test_parse_quote(self, source: str, begin: int, expected: tuple[int, str]) -> None:
+		parser = TokenParser2()
+		actual = parser.parse_quote(source, begin)
+		self.assertEqual(expected, actual)
+
+	@data_provider([
+		('abc', 0, (3, 'abc')),
+		('Abc', 0, (3, 'Abc')),
+		('_Abc', 0, (4, '_Abc')),
+		('_A_Bc0', 0, (6, '_A_Bc0')),
+		('a.b.c', 2, (3, 'b')),
+		('0 + a.b()', 6, (7, 'b')),
+	])
+	def test_parse_identifier(self, source: str, begin: int, expected: tuple[int, str]) -> None:
+		parser = TokenParser2()
+		actual = parser.parse_identifier(source, begin)
+		self.assertEqual(expected, actual)
+
+	@data_provider([
+		('1 + 2', 2, (3, '+')),
+		('1 +-2', 2, (3, '+')),
+		('a = 1', 2, (3, '=')),
+		('a += 1', 2, (4, '+=')),
+		('a(**b)', 1, (2, '(')),
+		('a(**b)', 2, (4, '**')),
+		('a == b', 2, (4, '==')),
+		('@a', 0, (1, '@')),
+		('!a', 0, (1, '!')),
+		('a;', 1, (2, ';')),
+		('[:b]', 1, (2, ':')),
+	])
+	def test_parse_symbol(self, source: str, begin: int, expected: tuple[int, str]) -> None:
+		parser = TokenParser2()
+		actual = parser.parse_symbol(source, begin)
+		self.assertEqual(expected, actual)
 
 
 class TestTokenParser(TestCase):

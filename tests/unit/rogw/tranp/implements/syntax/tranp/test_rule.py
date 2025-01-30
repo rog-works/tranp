de4a132a53eac4c47818baed87f2df0f0370e200
rog-works <rogangelf@gmail.com>
2025-01-31 08:19:29 +0900

    test: テストデータを追加

--- tests/unit/rogw/tranp/implements/syntax/tranp/test_rule.py
+++ tests/unit/rogw/tranp/implements/syntax/tranp/test_rule.py
@@ -14,9 +14,72 @@ class TestRules(TestCase):
 					('symbol', 'entry'),
 					('symbol', 'exp'),
 				]),
+				('rule', [
+					('expand', '?'),
+					('symbol', 'exp'),
+					('symbol', 'primary'),
+				]),
+				('rule', [
+					('expand', '?'),
+					('symbol', 'primary'),
+					('terms_or', [
+						('symbol', 'relay'),
+						('symbol', 'invoke'),
+						('symbol', 'indexer'),
+						('symbol', 'atom'),
+					]),
+				]),
+				('rule', [
+					('__empty__', ''),
+					('symbol', 'relay'),
+					('terms', [
+						('symbol', 'primary'),
+						('string', '"."'),
+						('symbol', 'name'),
+					]),
+				]),
+				('rule', [
+					('__empty__', ''),
+					('symbol', 'invoke'),
+					('terms', [
+						('symbol', 'primary'),
+						('string', '"("'),
+						('expr_opt', [
+							('symbol', 'args'),
+						]),
+						('string', '")"'),
+					]),
+				]),
+				('rule', [
+					('__empty__', ''),
+					('symbol', 'indexer'),
+					('terms', [
+						('symbol', 'primary'),
+						('string', '"["'),
+						('symbol', 'exp'),
+						('string', '"]"'),
+					]),
+				]),
+				('rule', [
+					('__empty__', ''),
+					('symbol', 'args'),
+					('terms', [
+						('symbol', 'exp'),
+						('expr_rep', [
+							('symbol', 'exp'),
+							('repeat', '*'),
+						]),
+					]),
+				]),
 			]),
 			'\n'.join([
 				'entry := exp',
+				'?exp := primary',
+				'?primary := relay | invoke | indexer | atom',
+				'relay := primary "." name',
+				'invoke := primary "(" [args] ")"',
+				'indexer := primary "[" exp "]"',
+				'args := exp (exp)*',
 			]),
 		),
 	])

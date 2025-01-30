6a76adb432a62b80c959b1590923da3bfb0a528e
rog-works <rogangelf@gmail.com>
2025-01-31 07:35:04 +0900

    test: Rulesのテストケース実装

--- tests/unit/rogw/tranp/implements/syntax/tranp/test_rule.py
+++ tests/unit/rogw/tranp/implements/syntax/tranp/test_rule.py
@@ -1,7 +1,26 @@
-from rogw.tranp.implements.syntax.tranp.ast import ASTTree
+from unittest import TestCase
+
+from rogw.tranp.implements.syntax.tranp.ast import ASTToken, ASTTree
 from rogw.tranp.implements.syntax.tranp.rule import Rules
+from rogw.tranp.implements.syntax.tranp.token import Token, TokenTypes
+from rogw.tranp.test.helper import data_provider
 
 
-class TestRules:
-	def from_ast(self, tree: ASTTree) -> None:
+class TestRules(TestCase):
+	@data_provider([
+		(
+			ASTTree('entry', [
+				ASTTree('rule', [
+					ASTToken.empty(),
+					ASTToken('symbol', Token(TokenTypes.Name, 'entry')),
+					ASTToken('symbol', Token(TokenTypes.Name, 'exp')),
+				]),
+			]),
+			'\n'.join([
+				'entry := exp',
+			]),
+		),
+	])
+	def test_from_ast(self, tree: ASTTree, expected: str) -> None:
 		rules = Rules.from_ast(tree)
+		self.assertEqual(expected, rules.pretty())

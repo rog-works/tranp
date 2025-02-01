44ec345d5bfd696e19895130f90a0fd2ffd1dfe4
rog-works <rogangelf@gmail.com>
2025-02-02 07:36:18 +0900

    refactor: ASTTokenを通常クラスに変更

--- rogw/tranp/implements/syntax/tranp/ast.py
+++ rogw/tranp/implements/syntax/tranp/ast.py
@@ -7,11 +7,20 @@ TupleTree: TypeAlias = tuple[str, list['TupleToken | TupleTree']]
 TupleEntry: TypeAlias = TupleToken | TupleTree
 
 
-class ASTToken(NamedTuple):
+class ASTToken:
 	"""AST(トークン)"""
 
-	name: str
-	value: Token
+	def __init__(self, name: str, value: Token) -> None:
+		self._name = name
+		self._value = value
+
+	@property
+	def name(self) -> str:
+		return self._name
+
+	@property
+	def value(self) -> Token:
+		return self._value
 
 	@classmethod
 	def empty(cls) -> 'ASTToken':
@@ -25,6 +34,29 @@ class ASTToken(NamedTuple):
 	def pretty(self, indent: str = '  ') -> str:
 		"""Returns: フォーマット書式"""
 		return f"('{self.name}', '{self.value.string}')"
+
+	def __repr__(self) -> str:
+		"""Returns: シリアライズ表現"""
+		return f'<ASTToken[{self.name}]: "{self.value.string}">'
+
+	def __str__(self) -> str:
+		"""Returns: 文字列表現"""
+		return f'("{self.name}", "{self.value.string}")'
+
+	def __hash__(self) -> int:
+		"""Returns: ハッシュ値"""
+		return hash(self.__repr__())
+
+	def __eq__(self, other: 'ASTToken | tuple[str, str]') -> bool:
+		"""Args: other: 比較対象 Returns: True = 一致"""
+		if isinstance(other, ASTToken):
+			return self.name == other.name and self.value == other.value
+		else:
+			return self.name == other[0] and self.value.string == other[1]
+
+	def __ne__(self, other: 'ASTToken | tuple[str, str]') -> bool:
+		"""Args: other: 比較対象 Returns: True = 一致"""
+		return not self.__eq__(other)
 
 
 class ASTTree(NamedTuple):

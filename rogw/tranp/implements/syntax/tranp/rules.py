e83958d08688e8c99059240505f8663ce8c49f63
rog-works <rogangelf@gmail.com>
2025-02-01 11:57:04 +0900

    refactor: ASTからの生成に変更

--- rogw/tranp/implements/syntax/tranp/rules.py
+++ rogw/tranp/implements/syntax/tranp/rules.py
@@ -1,4 +1,4 @@
-from rogw.tranp.implements.syntax.tranp.rule import Operators, Pattern, Patterns, Repeators, Rules
+from rogw.tranp.implements.syntax.tranp.rule import Rules
 from rogw.tranp.implements.syntax.tranp.tokenizer import TokenDefinition, Tokenizer
 
 
@@ -137,55 +137,119 @@ def grammar_rules() -> Rules:
 
 	Returns:
 		ルール一覧
-	Note:
-		```
-		entry := (rule)+
-		rule := [expand] symbol ":=" expr "\n"
-		?expr := terms_or
-		?terns_or := (terms "|")* terms
-		?terms := (term)* term
-		?term := symbol | string | regexp | expr_opt | expr_rep
-		expr_opt := "[" expr "]"
-		expr_rep := "(" expr ")" [repeat]
-		symbol := /[a-zA-Z_][0-9a-zA-Z_]*/
-		string := /"[^"]+"/
-		regexp := /\\/[^\\/]+\\//
-		repeat := /[*+?]/
-		expand := "?"
-		```
 	"""
-	return Rules({
-		'entry': Patterns([Pattern.S('rule')], rep=Repeators.OverOne),
-		'rule': Patterns([Patterns([Pattern.S('expand')], rep=Repeators.OneOrEmpty), Pattern.S('symbol'), Pattern.T('":="'), Pattern.S('expr'), Pattern.T('"\n"')]),
-		'?expr': Pattern.S('terms_or'),
-		'?terms_or': Patterns([
-			Patterns([Pattern.S('terms'), Pattern.T('"|"')], rep=Repeators.OverZero),
-			Pattern.S('terms'),
-		]),
-		'?terms': Patterns([
-			Patterns([Pattern.S('term')], rep=Repeators.OverZero),
-			Pattern.S('term'),
-		]),
-		'?term': Patterns([
-			Pattern.S('symbol'),
-			Pattern.S('string'),
-			Pattern.S('regexp'),
-			Pattern.S('expr_opt'),
-			Pattern.S('expr_rep'),
-		], op=Operators.Or),
-		'expr_opt': Patterns([
-			Pattern.T('"["'), Pattern.S('expr'), Pattern.T('"]"'),
-		]),
-		'expr_rep': Patterns([
-			Pattern.T('"("'), Pattern.S('expr'), Pattern.T('")"'),
-			Patterns([Pattern.S('repeat')], rep=Repeators.OneOrEmpty),
-		]),
-		'symbol': Pattern.T('/[a-zA-Z_][0-9a-zA-Z_]*/'),
-		'string': Pattern.T('/"[^"]+"/'),
-		'regexp': Pattern.T('/[\\/].+[\\/]/'),
-		'repeat': Pattern.T('/[*+?]/'),
-		'expand': Pattern.T('"?"'),
-	})
+	return Rules.from_ast(
+		('entry', [
+			('rule', [
+				('__empty__', ''),
+				('symbol', 'entry'),
+				('expr_rep', [
+					('symbol', 'rule'),
+					('repeat', '+')
+				])
+			]),
+			('rule', [
+				('__empty__', ''),
+				('symbol', 'rule'),
+				('terms', [
+					('expr_opt', [
+						('symbol', 'expand')
+					]),
+					('symbol', 'symbol'),
+					('string', '":="'),
+					('symbol', 'expr'),
+					('string', '"\n"')
+				])
+			]),
+			('rule', [
+				('expand', '?'),
+				('symbol', 'expr'),
+				('symbol', 'terms_or')
+			]),
+			('rule', [
+				('expand', '?'),
+				('symbol', 'terms_or'),
+				('terms', [
+					('expr_rep', [
+						('terms', [
+							('symbol', 'terms'),
+							('string', '"|"')
+						]),
+						('repeat', '*')
+					]),
+					('symbol', 'terms')
+				])
+			]),
+			('rule', [
+				('expand', '?'),
+				('symbol', 'terms'),
+				('terms', [
+					('expr_rep', [
+						('symbol', 'term'),
+						('repeat', '*')
+					]),
+					('symbol', 'term')
+				])
+			]),
+			('rule', [
+				('expand', '?'),
+				('symbol', 'term'),
+				('terms_or', [
+					('symbol', 'symbol'),
+					('symbol', 'string'),
+					('symbol', 'regexp'),
+					('symbol', 'expr_opt'),
+					('symbol', 'expr_rep')
+				])
+			]),
+			('rule', [
+				('__empty__', ''),
+				('symbol', 'expr_opt'),
+				('terms', [
+					('string', '"["'),
+					('symbol', 'expr'),
+					('string', '"]"')
+				])
+			]),
+			('rule', [
+				('__empty__', ''),
+				('symbol', 'expr_rep'),
+				('terms', [
+					('string', '"("'),
+					('symbol', 'expr'),
+					('string', '")"'),
+					('expr_opt', [
+						('symbol', 'repeat')
+					])
+				])
+			]),
+			('rule', [
+				('__empty__', ''),
+				('symbol', 'symbol'),
+				('regexp', '/[a-zA-Z_][0-9a-zA-Z_]*/')
+			]),
+			('rule', [
+				('__empty__', ''),
+				('symbol', 'string'),
+				('regexp', '/"[^"]+"/')
+			]),
+			('rule', [
+				('__empty__', ''),
+				('symbol', 'regexp'),
+				('regexp', '/[\\/].+[\\/]/')
+			]),
+			('rule', [
+				('__empty__', ''),
+				('symbol', 'repeat'),
+				('regexp', '/[*+?]/')
+			]),
+			('rule', [
+				('__empty__', ''),
+				('symbol', 'expand'),
+				('string', '"?"')
+			])
+		])
+	)
 
 
 def grammar_tokenizer() -> Tokenizer:
@@ -200,46 +264,3 @@ def grammar_tokenizer() -> Tokenizer:
 	definition.symbol = ''.join(definition.symbol.split('/'))
 	definition.pre_filters['comment_spaces'] = r'[ \t\f]*//.*$'
 	return Tokenizer(definition=definition)
-
-
-"""
-Note:
-```
-### EBNF
-[
-	'entry ::= exp',
-	'bool ::= /False|True/',
-	'int ::= /[1-9][0-9]*/',
-	'float ::= /(0|[1-9][0-9]*)[.][0-9]+/',
-	'str ::= /\'[^\']*\'|"[^"]*"/',
-	'none ::= "None"',
-	'var ::= /[a-zA-Z_][0-9a-zA-Z_]*/',
-	'exp ::= primary',
-	# FIXME 演算は一旦非対応
-	# 'exp ::= or_test',
-	# 'or_test ::= and_test ("or" and_test)*',
-	# 'and_test ::= not_test ("and" not_test_)*',
-	# 'not_test ::= "not" not_test | comparison',
-	# 'comparison ::= sum (comp_op sum)*',
-	# 'sum ::= term (add_op term)*',
-	# 'term ::= factor (mul_op factor)*',
-	# 'factor ::= unary_op factor | primary',
-	# 'comp_op ::= "<" | ">" | "==" | "<=" | ">=" | "!=" | "in" | "not" "in" | "is" | "is" "not"',
-	# 'add_op ::= "+" | "-"',
-	# 'mul_op ::= "*" | "/" | "%"',
-	# 'unary_op ::= "+" | "-"',
-	'primary ::= relay | invoke | indexer | atom',
-	'relay ::= primary "." var',
-	'invoke ::= primary "(" [args] ")"',
-	'indexer ::= primary "[" exp "]"',
-	'args ::= exp ("," exp)*',
-	'atom ::= var | bool | none | str | int | float',
-	# FIXME list | dict | group_exp は一旦非対応
-	# 'list ::= "[" [list_exps] "]"',
-	# 'dict ::= "{" [dict_exps] "}"',
-	# 'group_exp ::= "(" exp ")"',
-	# 'list_exps ::= exp ("," exp)*',
-	# 'dict_exps ::= dict_pair ("," dict_pair)*',
-	# 'dict_pair ::= exp ":" exp',
-]
-"""

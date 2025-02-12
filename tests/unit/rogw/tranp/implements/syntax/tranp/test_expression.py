0df4135bc6a4d961e7580d36d67f45d65a65e2d0
rog-works <rogangelf@gmail.com>
2025-02-12 12:53:34 +0900

    test: Expressionのテストデータを追加

--- tests/unit/rogw/tranp/implements/syntax/tranp/test_expression.py
+++ tests/unit/rogw/tranp/implements/syntax/tranp/test_expression.py
@@ -1,17 +1,42 @@
 from unittest import TestCase
 
-from rogw.tranp.implements.syntax.tranp.expression import ExpressionTerminal
+from rogw.tranp.implements.syntax.tranp.expression import ExpressionSymbol, ExpressionTerminal
 from rogw.tranp.implements.syntax.tranp.rule import Pattern
-from rogw.tranp.implements.syntax.tranp.state import Context, Trigger, Triggers
+from rogw.tranp.implements.syntax.tranp.state import Context, State, States, Trigger, Triggers
 from rogw.tranp.implements.syntax.tranp.token import Token, TokenTypes
 from rogw.tranp.test.helper import data_provider
 
 
 class TestExpressionTerminal(TestCase):
 	@data_provider([
-		(r'/\w+/', Token(TokenTypes.Name, 'a'), Triggers.FinishStep),
+		('"\n"', 0, Token(TokenTypes.NewLine, '\n'), Triggers.FinishStep),
+		('"\n"', 0, Token(TokenTypes.Name, 'a'), Triggers.Abort),
+		('"\n"', 1, Token(TokenTypes.Name, 'a'), Triggers.Empty),
+		(r'/[a-zA-Z_]\w*/', 0, Token(TokenTypes.Name, 'a'), Triggers.FinishStep),
+		(r'/[a-zA-Z_]\w*/', 0, Token(TokenTypes.Digit, '0'), Triggers.Abort),
+		(r'/[a-zA-Z_]\w*/', 1, Token(TokenTypes.Name, 'a'), Triggers.Empty),
 	])
-	def test_step(self, expression: str, token: Token, expected: Trigger) -> None:
+	def test_step(self, expression: str, cursor: int, token: Token, expected: Trigger) -> None:
 		instance = ExpressionTerminal(Pattern.make(expression))
-		actual = instance.step(Context.new(0, {}), 0, token)
+		actual = instance.step(Context.new(cursor, {}), 0, token)
+		self.assertEqual(expected, actual)
+
+
+class TestExpressionSymbol(TestCase):
+	@data_provider([
+		('hoge', 0, States.Idle, Triggers.Empty),
+		('hoge', 0, States.Step, Triggers.Empty),
+		('hoge', 0, States.FinishSkip, Triggers.FinishSkip),
+		('hoge', 0, States.FinishStep, Triggers.FinishStep),
+		('hoge', 0, States.UnfinishSkip, Triggers.UnfinishSkip),
+		('hoge', 0, States.UnfinishStep, Triggers.UnfinishStep),
+		('hoge', 0, States.Abort, Triggers.Abort),
+		('hoge', 1, States.Idle, Triggers.Empty),
+		('hoge', 1, States.Step, Triggers.Empty),
+		('hoge', 1, States.FinishStep, Triggers.Empty),
+		('hoge', 1, States.Abort, Triggers.Empty),
+	])
+	def test_accept(self, symbol: str, cursor: int, on_state: State, expected: Trigger) -> None:
+		instance = ExpressionSymbol(Pattern.make(symbol))
+		actual = instance.accept(Context.new(cursor, {}), 0, lambda name, state: name == symbol and state == on_state)
 		self.assertEqual(expected, actual)

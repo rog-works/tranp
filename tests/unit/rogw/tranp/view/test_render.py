153041bf26e366008ae03d9199da30c2216e342e
rog-works <rogangelf@gmail.com>
2025-10-10 12:51:42 +0900

    test: #430 Lambdaのテンプレートを修正

--- tests/unit/rogw/tranp/view/test_render.py
+++ tests/unit/rogw/tranp/view/test_render.py
@@ -1342,7 +1342,9 @@ class TestRenderer(TestCase):
 		self.assertRender(f'indexer/{spec}', vars, expected)
 
 	@data_provider([
-		({'expression': '1', 'var_type': 'int'}, '[&]() -> int { return 1; }'),
+		({'params': [], 'expression': '1', 'var_type': 'int'}, '[&]() -> int { return 1; }'),
+		({'params': ['int a'], 'expression': '', 'var_type': 'void'}, '[&](int a) -> void {}'),
+		({'params': ['int a', 'bool b'], 'expression': '', 'var_type': 'void'}, '[&](int a, bool b) -> void {}'),
 	])
 	def test_render_lambda(self, vars: dict[str, Any], expected: str) -> None:
 		self.assertRender('lambda', vars, expected)

cfbb919f597f5e6f547ee0aff4f5ca2a3a9fae78
rog-works <rogangelf@gmail.com>
2025-11-17 17:55:13 +0900

    feat: #517 右再帰パーサーに修正

--- rogw/tranp/implements/syntax/tranp/syntax.py
+++ rogw/tranp/implements/syntax/tranp/syntax.py
@@ -3,7 +3,7 @@ from typing import NamedTuple
 
 from rogw.tranp.dsn.dsn import DSN
 from rogw.tranp.implements.syntax.tranp.ast import ASTEntry, ASTToken, ASTTree
-from rogw.tranp.implements.syntax.tranp.rule import Comps, Unwraps, Operators, Pattern, PatternEntry, Patterns, Repeators, Roles, Rules
+from rogw.tranp.implements.syntax.tranp.rule import Comps, Operators, Pattern, PatternEntry, Patterns, Repeators, Roles, Rules, Unwraps
 from rogw.tranp.implements.syntax.tranp.tokenizer import ITokenizer, Token, Tokenizer
 from rogw.tranp.lang.convertion import as_a
 
@@ -60,55 +60,26 @@ class Context(NamedTuple):
 	"""解析コンテキスト"""
 
 	position: int
-	minimum: int
-	maximum: int
-	recursive_symbol: str
 
 	@classmethod
 	def start(cls) -> 'Context':
 		"""Returns: 開始時のインスタンス"""
-		return cls(0, -1, -1, '')
+		return cls(0)
 
 	@property
 	def cursor(self) -> int:
 		"""Returns: 参照位置"""
-		return self.position + max(0, self.minimum)
-
-	@property
-	def accepted_recursive(self) -> bool:
-		"""Returns: True = 左再帰を受け入れ"""
-		return self.minimum == -1 and self.maximum == -1 and len(self.recursive_symbol) == 0
+		return self.position
 
 	def step(self, steps: int) -> 'Context':
-		"""ステップ数を加えて新規作成。ステップ数が1以上の時は暗黙的に読み取り位置の制限を解除
+		"""ステップ数を加えて新規作成
 
 		Args:
 			steps: 追加の進行ステップ数
 		Returns:
 			インスタンス
-		Note:
-			### ステップ進行時の制限解除の必然性
-			* 読み取り位置の制限は実質的に無限再帰の抑制ために存在
-			* 無限再帰が起きる条件は「同条件の繰り返し」であり、逆に言えば条件が変われば無限再帰にはならない
-			* ステップの進行=条件の変化であり、制限の解除によって通常の解析条件に戻るだけで悪影響はない
 		"""
-		if steps == 0:
-			return Context(self.position + steps, self.minimum, self.maximum, self.recursive_symbol)
-		else:
-			return Context(self.position + steps + max(0, self.minimum), -1, -1, '')
-
-	def block_step(self, steps: int = 0, minimum: int = -1, maximum: int = -1, recursive_symbol: str = '') -> 'Context':
-		"""ステップ数を加えて新規作成。作成したコンテキスト上では左再帰がブロックされる
-
-		Args:
-			steps: 追加の進行ステップ数 (default = 0)
-			minimum: 読み取り位置の下限 (default = -1)
-			maximum: 読み取り位置の上限 (default = -1)
-			recursive_symbol: 再帰を開始したルール名(=読み取り下限の条件) (default = -1)
-		Returns:
-			インスタンス
-		"""
-		return Context(self.position + steps, minimum, maximum, recursive_symbol)
+		return Context(self.position + steps)
 
 
 class SyntaxParser:
@@ -139,8 +110,8 @@ class SyntaxParser:
 		length = len(tokens)
 		step, entry = self._match_symbol(tokens, Context.start(), entrypoint)
 		if step.steps != length:
-			message = ErrorCollector(source, tokens, step.steps).summary()
-			raise ValueError(f'Syntax parse error. Last token not reached. {message}')
+			message = ErrorCollector(source, tokens, length - 1 - step.steps).summary()
+			raise ValueError(f'Syntax parse error. First token not reached. {message}')
 
 		return as_a(ASTTree, entry)
 
@@ -204,8 +175,6 @@ class SyntaxParser:
 				return self._match_repeat(tokens, context, pattern, route)
 			elif pattern.op == Operators.Or:
 				return self._match_or(tokens, context, pattern, route)
-			elif pattern.op == Operators.And and context.accepted_recursive and self.rules.recursive_of(pattern[0]):
-				return self._match_and_recursive(tokens, context, pattern, route)
 			else:
 				return self._match_and(tokens, context, pattern, route)
 		else:
@@ -245,19 +214,12 @@ class SyntaxParser:
 		Returns:
 			(ステップ, ASTエントリーリスト)
 		"""
-		# XXX 読み取り位置の上限を考慮(左再帰時) @see _match_and_recursive_first
-		ok_limit = context.maximum == -1 or patterns.min_size <= context.maximum
-		ok_range = context.position + patterns.min_size <= len(tokens)
-		if not (ok_limit and ok_range):
-			return Step.ng(), []
-
 		steps = 0
 		children: list[ASTEntry] = []
-		for index, pattern in enumerate(patterns):
-			# XXX 読み取り位置の下限を考慮(左再帰時) @see _match_and_recursive_extend
-			if index < context.minimum and isinstance(pattern, Pattern) and pattern.expression == context.recursive_symbol:
-				continue
-
+		length = len(patterns)
+		for i in range(length):
+			index = length - 1 - i
+			pattern = patterns[index]
 			in_step, in_children = self._match_entry(tokens, context.step(steps), pattern, route)
 			if not in_step.steping:
 				return Step.ng(), []
@@ -265,150 +227,7 @@ class SyntaxParser:
 			children.extend(in_children)
 			steps += in_step.steps
 
-		return Step.ok(steps), children
-
-	def _match_and_recursive(self, tokens: list[Token], context: Context, patterns: Patterns, route: str) -> tuple[Step, list[ASTEntry]]:
-		"""パターングループ(AND/左再帰)を検証し、子のASTエントリーを生成
-
-		Args:
-			tokens: トークンリスト
-			context: 解析コンテキスト
-			patterns: マッチングパターングループ
-			route: 探索ルート
-		Returns:
-			(ステップ, ASTエントリーリスト)
-		Note:
-			```
-			### 左再帰の成立条件
-			* AND条件のルールの先頭要素が左再帰の場合が対象
-			### 左再帰の否定条件
-			* 先頭要素以外の再帰 (同条件の繰り返しにならず無限ループに陥らないため対処が不要)
-			* 1要素しか存在しない場合の再帰 (構文的に意味が無い)
-			* OR条件に単独で存在する再帰 (グループ化してAND条件にすることで対処が可能なため非対応)
-			```
-		Examples:
-			```
-			recursive := rule
-			// OKパターン
-			rule := recursive "." name
-			rule := (recursive) | other | ...
-			// NGパターン
-			rule := "@" recursive ...
-			rule := recursive
-			rule := recursive | other | ...
-			```
-		"""
-		first_step, first_children = self._match_and_recursive_first(tokens, context, patterns, route)
-		if not first_step.steping:
-			return Step.ng(), []
-
-		extend_step, extend_children = self._match_and_recursive_extend(tokens, context, patterns, route, first_children)
-		if not extend_step.steping:
-			return Step.ng(), []
-
-		symbol = DSN.right(route, 1)
-		if extend_children[0].name != symbol:
-			return Step.ng(), []
-
-		return Step.ok(extend_step.steps + 1), as_a(ASTTree, extend_children[0]).children
-
-	def _match_and_recursive_first(self, tokens: list[Token], context: Context, patterns: Patterns, route: str) -> tuple[Step, list[ASTEntry]]:
-		"""パターングループ(AND/左再帰/先頭要素)を検証し、子のASTエントリーを生成
-
-		Args:
-			tokens: トークンリスト
-			context: 解析コンテキスト
-			patterns: マッチングパターングループ
-			route: 探索ルート
-		Returns:
-			(ステップ, ASTエントリーリスト)
-		Note:
-			```
-			* 最小単位である1トークンでマッチングし、それを基点とする
-			* 2トークン以上が最小単位だった場合は必ず失敗する XXX 要検討
-			```
-		"""
-		first_pattern = as_a(Pattern, patterns[0])
-		first_context = context.block_step(maximum=1)
-		step, children = self._match_entry(tokens, first_context, first_pattern, route)
-		if not step.steping:
-			return Step.ng(), []
-
-		return step, [self._unwrap_recursive_children(first_pattern, route, children)]
-	
-	def _match_and_recursive_extend(self, tokens: list[Token], context: Context, patterns: Patterns, route: str, step_children: list[ASTEntry]) -> tuple[Step, list[ASTEntry]]:
-		"""パターングループ(AND/左再帰/先頭要素の拡張)を検証し、子のASTエントリーを生成
-
-		Args:
-			tokens: トークンリスト
-			context: 解析コンテキスト
-			patterns: マッチングパターングループ
-			route: 探索ルート
-			step_children: 前段で解析済みのASTエントリーリスト
-		Returns:
-			(ステップ, ASTエントリーリスト)
-		Note:
-			```
-			### 処理内容
-			* マッチングの基点である先頭要素をスキップし、後続の要素とのマッチングを繰り返す
-			* 本来のASTの構造から先頭要素が欠けた状態でマッチングするため、ASTを再構築
-			* 繰り返す度に直前のレスポンスを内側にスタック
-			### 拡張の条件
-			* マッチングパターンは必ずグループである前提 (左再帰の成立条件) @see _match_and_recursive
-			* 結果が期待通りの場合、必ずASTTreeが得られる
-			* これに該当しない結果を得た場合はマッチング失敗と見做す (構文上の曖昧性があり、これは避けられない)
-			```
-		"""
-		recursive_symbol = DSN.elements(route)[-2]
-		first_pattern = as_a(Pattern, patterns[0])
-		steps = 0
-		children = step_children.copy()
-		while context.cursor + steps < len(tokens):
-			in_context = context.block_step(steps=steps, minimum=1, recursive_symbol=recursive_symbol)
-			in_step, in_children = self._match_entry(tokens, in_context, first_pattern, route)
-			if not in_step.steping:
-				break
-
-			# 解決したエントリーが再帰呼び出しを伴うルールから取得したものか確認 XXX 展開の仕様を考慮するとこの判定の確実性は怪しい。要検討
-			unwrap_entry = self._unwrap_recursive_children(first_pattern, route, in_children)
-			entry_rule = self.rules[unwrap_entry.name]
-			if not isinstance(entry_rule, Patterns) or recursive_symbol not in entry_rule.symbols:
-				# XXX relayを前提に考えた場合、除外したいエントリーはatomのみ
-				# XXX それ以外の条件で1件以上マッチしていたならばそこまでは正なのでchildrenは削除しない。が正しいはず
-				# children.clear()
-				break
-
-			unwrap_tree = as_a(ASTTree, unwrap_entry)
-			expect_tree = ASTTree(unwrap_tree.name, [*children, *unwrap_tree.children])
-			children: list[ASTEntry] = [expect_tree]
-			steps += in_step.steps
-
-		if len(children) == 0:
-			return Step.ng(), []
-
-		return Step.ok(steps), children
-
-	def _unwrap_recursive_children(self, first_pattern: Pattern, route: str, children: list[ASTEntry]) -> ASTEntry:
-		"""再帰処理によって取得したASTエントリーから不要な階層を展開
-
-		Args:
-			first_pattern: マッチングパターン(先頭要素)
-			route: 探索ルート
-			children: 再帰処理中のASTエントリーリスト
-		Returns:
-			ASTツリー
-		"""
-		begin = route.rfind(first_pattern.expression)
-		count = DSN.elem_counts(route[begin:])
-		entry = children[0]
-		candidates: list[str] = []
-		for index in range(count - 1):
-			if index == 0 or entry.name in candidates:
-				assert isinstance(entry, ASTTree) and len(entry.children) == 1
-				candidates = as_a(Patterns, self.rules[entry.name]).symbols
-				entry = entry.children[0]
-
-		return entry
+		return Step.ok(steps), list(reversed(children))
 
 	def _match_repeat(self, tokens: list[Token], context: Context, patterns: Patterns, route: str) -> tuple[Step, list[ASTEntry]]:
 		"""パターングループ(リピート)を検証し、子のASTエントリーを生成
@@ -462,7 +281,7 @@ class SyntaxParser:
 		if len(tokens) <= context.cursor:
 			return Step.ng(), Token.empty()
 
-		token = tokens[context.cursor]
+		token = tokens[len(tokens) - 1 - context.cursor]
 		ok = self._compare_token(token, pattern)
 		return (Step.ok(1), token) if ok else (Step.ng(), Token.empty())
 	

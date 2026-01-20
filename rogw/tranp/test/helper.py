5cf5d7ac245542cb9ab904eaf089dfad383faf84
rog-works <rogangelf@gmail.com>
2026-01-21 08:29:02 +0900

    refactor: data_providerのfunctools.wraps対応

--- rogw/tranp/test/helper.py
+++ rogw/tranp/test/helper.py
@@ -1,3 +1,4 @@
+import functools
 from collections.abc import Callable
 from typing import ParamSpec, TypeVar
 
@@ -15,6 +16,7 @@ def data_provider(args_list: list[tuple], *includes: int) -> Callable:
 		デコレーター
 	"""
 	def decorator(test_func: Callable):
+		@functools.wraps(test_func)
 		def wrapper(self, *_):
 			for index, provide_args in enumerate(args_list):
 				if len(includes) and index not in includes:

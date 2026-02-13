0b32a4a679087c80cd7289ac8f4c7faccd5fd89f
rog-works <rogangelf@gmail.com>
2026-02-13 11:07:17 +0900

    feat: #538 ReferenceTypeを追加

--- rogw/tranp/compatible/libralies/weakref.py
+++ rogw/tranp/compatible/libralies/weakref.py
@@ -1,4 +1,10 @@
 from collections.abc import Callable
+from typing import Generic, TypeVar
+
+T = TypeVar('T')
+
+
+class ReferenceType(Generic[T]): ...
 
 
 def finalize(obj: object, callback: Callable[[], None]) -> None: ...

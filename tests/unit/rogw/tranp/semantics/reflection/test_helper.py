5c341d44e3a5677229e856d67985a9b77e7246af
rog-works <rogangelf@gmail.com>
2026-02-13 18:01:52 +0900

    test: #538 コードの整理

--- tests/unit/rogw/tranp/semantics/reflection/test_helper.py
+++ tests/unit/rogw/tranp/semantics/reflection/test_helper.py
@@ -4,50 +4,9 @@ from rogw.tranp.dsn.dsn import DSN
 from rogw.tranp.test.helper import data_provider
 
 
-class TestTemplateManipulator(TestCase):
-	@data_provider([
-		(
-			### schema: CP<T_co>
-			# [CP, T_co]
-			'parameters.0.0',
-			{
-				'klass.0': ['CWP', 'T_co'],
-				'parameters.0.0': ['CP', 'T_co'],
-			},
-			{
-				### actual: Union<CP<Promise>, None>
-				# CWP<T_co>
-				'klass.0': ['CWP', 'T_co'],
-				# Union<CP<Promise>, None>
-				'parameters.0.0.0': ['CP', 'Promise'],
-				'parameters.0.1': ['None'],
-			},
-			'parameters.0.0.0',
-		),
-	])
-	def test_update_path(self, target: str, schema_props: dict[str, list[str]], actual_props: dict[str, list[str]], expected: str) -> None:
-		actual = self.find_actual_path(target, schema_props[target], actual_props)
-		self.assertEqual(expected, actual)
-
-	@data_provider([
-		(
-			{
-				'parameters.0': 'Union',
-				'parameters.0.0': 'CP',
-				'parameters.0.0.0': 'Promise',
-				'parameters.0.1': 'None',
-			},
-			{
-				'parameters.0.0.0': ['Union', 'CP', 'Promise'],
-				'parameters.0.1': ['Union', 'None'],
-			},
-		),
-	])
-	def test_normalize(self, props: dict[str, str], expected: dict[str, list[str]]) -> None:
-		actual = self.normalize_props(props)
-		self.assertEqual(expected, actual)
-
-	def normalize_props(self, props: dict[str, str]) -> dict[str, list[str]]:
+class Helper:
+	@classmethod
+	def normalize_props(cls, props: dict[str, str], ignore: list[str]) -> dict[str, list[str]]:
 		unique_keys: list[str] = []
 		keys = list(props.keys())
 		for i, key in enumerate(keys):
@@ -65,14 +24,15 @@ class TestTemplateManipulator(TestCase):
 			count = DSN.elem_counts(key)
 			for i in range(2, count):
 				begin = DSN.left(key, i)
-				if begin in props:
+				if begin in props and props[begin] not in ignore:
 					normalized[key].append(props[begin])
 
 			normalized[key].append(props[key])
 
 		return normalized
 
-	def find_actual_path(self, target: str, target_props: list[str], actual_props: dict[str, list[str]]) -> str:
+	@classmethod
+	def find_actual_path(cls, target: str, target_props: list[str], actual_props: dict[str, list[str]]) -> str:
 		target_begin = DSN.left(target, 2)
 		target_begins = target_props[:-1]
 		for actual_path, props in actual_props.items():
@@ -80,3 +40,52 @@ class TestTemplateManipulator(TestCase):
 				return actual_path
 
 		return ''
+
+
+class TestTemplateManipulator(TestCase):
+	@data_provider([
+		(
+			{
+				'parameters.0': 'Union',
+				'parameters.0.0': 'CP',
+				'parameters.0.0.0': 'Promise',
+				'parameters.0.1': 'None',
+			},
+			['Union'],
+			{
+				'parameters.0.0.0': ['CP', 'Promise'],
+				'parameters.0.1': ['None'],
+			},
+		),
+	])
+	def test_normalize_props(self, props: dict[str, str], ignore: list[str], expected: dict[str, list[str]]) -> None:
+		actual = Helper.normalize_props(props, ignore)
+		self.assertEqual(expected, actual)
+
+	@data_provider([
+		(
+			'parameters.0.0',
+			### schema: CP<T_co>
+			{
+				'klass': 'CWP',
+				'klass.0': 'T_co',
+				'parameters.0': 'CP',
+				'parameters.0.0': 'T_co',
+			},
+			### actual: Union<CP<Promise>, None>
+			{
+				'klass': 'CWP',
+				'klass.0': 'T_co',
+				'parameters.0': 'Union',
+				'parameters.0.0': 'CP',
+				'parameters.0.0.0': 'Promise',
+				'parameters.0.1': 'None',
+			},
+			'parameters.0.0.0',
+		),
+	])
+	def test_find_actual_path(self, target: str, schema_props: dict[str, str], actual_props: dict[str, str], expected: str) -> None:
+		normalize_schema_props = Helper.normalize_props(schema_props, ignore=['Union'])
+		normalize_actual_props = Helper.normalize_props(actual_props, ignore=['Union'])
+		actual = Helper.find_actual_path(target, normalize_schema_props[target], normalize_actual_props)
+		self.assertEqual(expected, actual)

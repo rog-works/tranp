3f87a9b108120c00629a61fdabdaad74c79d1b60
rog-works <rogangelf@gmail.com>
2026-02-13 17:52:30 +0900

    test: #538 normalizeの検証＆find_actual_propsを改善

--- tests/unit/rogw/tranp/semantics/reflection/test_helper.py
+++ tests/unit/rogw/tranp/semantics/reflection/test_helper.py
@@ -1,8 +1,6 @@
 from unittest import TestCase
 
-import rogw.tranp.syntax.node.definition as defs
 from rogw.tranp.dsn.dsn import DSN
-from rogw.tranp.syntax.node.node import Node
 from rogw.tranp.test.helper import data_provider
 
 
@@ -10,49 +8,75 @@ class TestTemplateManipulator(TestCase):
 	@data_provider([
 		(
 			### schema: CP<T_co>
+			# [CP, T_co]
 			'parameters.0.0',
+			{
+				'klass.0': ['CWP', 'T_co'],
+				'parameters.0.0': ['CP', 'T_co'],
+			},
 			{
 				### actual: Union<CP<Promise>, None>
 				# CWP<T_co>
-				'klass': defs.Class,
-				'klass.0': defs.Class,
+				'klass.0': ['CWP', 'T_co'],
 				# Union<CP<Promise>, None>
-				'parameters.0': defs.UnionType,
-				'parameters.0.0': defs.Class,
-				'parameters.0.0.0': defs.Class,
-				'parameters.0.1': defs.NullType,
+				'parameters.0.0.0': ['CP', 'Promise'],
+				'parameters.0.1': ['None'],
 			},
 			'parameters.0.0.0',
 		),
 	])
-	def test_update_path(self, target: str, props: dict[str, type[Node]], expected: str) -> None:
-		actual = self.find_actual_path(target, props)
+	def test_update_path(self, target: str, schema_props: dict[str, list[str]], actual_props: dict[str, list[str]], expected: str) -> None:
+		actual = self.find_actual_path(target, schema_props[target], actual_props)
 		self.assertEqual(expected, actual)
 
-	def find_actual_path(self, target: str, props: dict[str, type[Node]]) -> str:
-		count = DSN.elem_counts(target)
-		if count == 1:
-			return target
+	@data_provider([
+		(
+			{
+				'parameters.0': 'Union',
+				'parameters.0.0': 'CP',
+				'parameters.0.0.0': 'Promise',
+				'parameters.0.1': 'None',
+			},
+			{
+				'parameters.0.0.0': ['Union', 'CP', 'Promise'],
+				'parameters.0.1': ['Union', 'None'],
+			},
+		),
+	])
+	def test_normalize(self, props: dict[str, str], expected: dict[str, list[str]]) -> None:
+		actual = self.normalize_props(props)
+		self.assertEqual(expected, actual)
 
-		begin = DSN.left(target, 2)
-		return self.__find_actual_path_internal(count - 1, props, begin, first=True)
+	def normalize_props(self, props: dict[str, str]) -> dict[str, list[str]]:
+		unique_keys: list[str] = []
+		keys = list(props.keys())
+		for i, key in enumerate(keys):
+			found = False
+			for j in range(i + 1, len(keys)):
+				if keys[j].startswith(key):
+					found = True
+					break
 
-	def __find_actual_path_internal(self, remain: int, props: dict[str, type[Node]], begin: str, first: bool = False) -> str:
-		if remain == 0:
-			return begin
+			if not found:
+				unique_keys.append(key)
 
-		i = 0
-		path = begin if first else DSN.join(begin, str(i))
-		while path in props:
-			next_remain = remain if props[path] is (defs.UnionType) else remain - 1
-			found = self.__find_actual_path_internal(next_remain, props, path)
-			if found:
-				return found
-			
-			if first:
-				break
+		normalized: dict[str, list[str]] = {key: [] for key in unique_keys}
+		for key in unique_keys:
+			count = DSN.elem_counts(key)
+			for i in range(2, count):
+				begin = DSN.left(key, i)
+				if begin in props:
+					normalized[key].append(props[begin])
 
-			i += 1
-			path = DSN.join(path, str(i))
+			normalized[key].append(props[key])
+
+		return normalized
+
+	def find_actual_path(self, target: str, target_props: list[str], actual_props: dict[str, list[str]]) -> str:
+		target_begin = DSN.left(target, 2)
+		target_begins = target_props[:-1]
+		for actual_path, props in actual_props.items():
+			if actual_path.startswith(target_begin) and len(target_props) == len(props) and target_begins == props[:-1]:
+				return actual_path
 
 		return ''

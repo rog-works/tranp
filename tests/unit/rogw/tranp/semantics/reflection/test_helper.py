e81b97e58a0313842be580003219a82247e49869
rog-works <rogangelf@gmail.com>
2026-02-13 18:27:32 +0900

    test: #538 マッチ条件の検証を追加

--- tests/unit/rogw/tranp/semantics/reflection/test_helper.py
+++ tests/unit/rogw/tranp/semantics/reflection/test_helper.py
@@ -33,17 +33,41 @@ class Helper:
 
 	@classmethod
 	def find_actual_path(cls, target: str, target_props: list[str], actual_props: dict[str, list[str]]) -> str:
-		target_begin = DSN.left(target, 2)
-		target_begins = target_props[:-1]
+		target_begin_path = DSN.left(target, 2)
+		target_begin_props = target_props[:-1]
 		for actual_path, props in actual_props.items():
-			if actual_path.startswith(target_begin) and len(target_props) == len(props) and target_begins == props[:-1]:
+			if not actual_path.startswith(target_begin_path):
+				continue
+			elif len(target_props) == len(props) and target_begin_props == props[:-1]:
 				return actual_path
+			elif len(target_props) < len(props) and target_begin_props == props[:len(target_begin_props)]:
+				lacks = len(props) - len(target_props)
+				return DSN.left(actual_path, DSN.elem_counts(actual_path) - lacks)
 
 		return ''
 
 
 class TestTemplateManipulator(TestCase):
 	@data_provider([
+		(
+			{
+				'parameters.0': 'Promise',
+			},
+			['Union'],
+			{
+				'parameters.0': ['Promise'],
+			},
+		),
+		(
+			{
+				'parameters.0': 'CP',
+				'parameters.0.0': 'Promise',
+			},
+			['Union'],
+			{
+				'parameters.0.0': ['CP', 'Promise'],
+			},
+		),
 		(
 			{
 				'parameters.0': 'Union',
@@ -82,6 +106,60 @@ class TestTemplateManipulator(TestCase):
 				'parameters.0.1': 'None',
 			},
 			'parameters.0.0.0',
+		),
+		(
+			'parameters.0.0.0',
+			### schema: Union<CP<T_co>, None>
+			{
+				'klass': 'CWP',
+				'klass.0': 'T_co',
+				'parameters.0': 'Union',
+				'parameters.0.0': 'CP',
+				'parameters.0.0.0': 'T_co',
+			},
+			### actual: CP<Promise>
+			{
+				'klass': 'CWP',
+				'klass.0': 'T_co',
+				'parameters.0': 'CP',
+				'parameters.0.0': 'Promise',
+			},
+			'parameters.0.0',
+		),
+		(
+			'parameters.0.0',
+			### schema: CP<T_co>
+			{
+				'klass': 'CWP',
+				'klass.0': 'T_co',
+				'parameters.0': 'CP',
+				'parameters.0.0': 'T_co',
+			},
+			### actual: CP<Promise>
+			{
+				'klass': 'CWP',
+				'klass.0': 'T_co',
+				'parameters.0': 'CP',
+				'parameters.0.0': 'Promise',
+			},
+			'parameters.0.0',
+		),
+		(
+			'parameters.0',
+			### schema: T_co
+			{
+				'klass': 'CWP',
+				'klass.0': 'T_co',
+				'parameters.0': 'T_co',
+			},
+			### actual: CP<Promise>
+			{
+				'klass': 'CWP',
+				'klass.0': 'T_co',
+				'parameters.0': 'CP',
+				'parameters.0.0': 'Promise',
+			},
+			'parameters.0',
 		),
 	])
 	def test_find_actual_path(self, target: str, schema_props: dict[str, str], actual_props: dict[str, str], expected: str) -> None:

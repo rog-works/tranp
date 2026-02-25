15f6a680b7b0f039afdca78135bad1cdbef46c20
rog-works <rogangelf@gmail.com>
2026-02-25 18:54:04 +0900

    fix: #551 不要な対応を削除

--- rogw/tranp/implements/transpiler/evaluator.py
+++ rogw/tranp/implements/transpiler/evaluator.py
@@ -124,10 +124,7 @@ class LiteralEvaluator:
 		return f'{quote}{left[1:-1]}{right[1:-1]}{quote}'
 
 	def on_argument(self, node: defs.Argument, label: Evaluator.Value, value: Evaluator.Value) -> Evaluator.Value:
-		if node.value.is_a(defs.Literal):
-			return value
-		else:
-			return self.exec(node.value)
+		return value
 
 	def on_argument_label(self, node: defs.ArgumentLabel) -> Evaluator.Value:
 		return node.tokens

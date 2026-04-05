0c715c258addda828bd6ef0aea79dbbf2c85a058
rog-works <rogangelf@gmail.com>
2026-04-05 10:54:20 +0900

    fix: #576 Number系をTypeAliasに変更

--- rogw/tranp/compatible/cpp/classes.py
+++ rogw/tranp/compatible/cpp/classes.py
@@ -1,115 +1,22 @@
-from typing import Self, TypeAlias
+from typing import TypeAlias
 
 # String
 
 class char(str): ...
 class wchar_t(str): ...
 
-# Digit
+# Number
 
-class digit(int):
-	"""int系統の基底クラス"""
+byte: TypeAlias = int
+int8: TypeAlias = int
+uint8: TypeAlias = int
+int32: TypeAlias = int
+int64: TypeAlias = int
+uint32: TypeAlias = int
+uint64: TypeAlias = int
+double: TypeAlias = float
 
-	def __hash__(self) -> int:
-		"""Returns: ハッシュ値"""
-		return self
-
-	def __eq__(self, other: int | Self) -> bool:
-		"""other: 対象 Returns: 演算結果"""
-		return int(self) == other
-
-	def __ne__(self, other: int | Self) -> bool:
-		"""other: 対象 Returns: 演算結果"""
-		return int(self) != other
-
-	def __lt__(self, other: int | Self) -> bool:
-		"""other: 対象 Returns: 演算結果"""
-		return int(self) < other
-
-	def __gt__(self, other: int | Self) -> bool:
-		"""other: 対象 Returns: 演算結果"""
-		return int(self) > other
-
-	def __le__(self, other: int | Self) -> bool:
-		"""other: 対象 Returns: 演算結果"""
-		return int(self) <= other
-
-	def __ge__(self, other: int | Self) -> bool:
-		"""other: 対象 Returns: 演算結果"""
-		return int(self) >= other
-
-	def __or__(self, other: int | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(int(self) | other)
-
-	def __xor__(self, other: int | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(int(self) ^ other)
-
-	def __and__(self, other: int | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(int(self) & other)
-
-	def __lshift__(self, n: int) -> Self:
-		"""n: シフト数 Returns: 演算結果"""
-		return self.__class__(int(self) << n)
-
-	def __rshift__(self, n: int) -> Self:
-		"""n: シフト数 Returns: 演算結果"""
-		return self.__class__(int(self) >> n)
-
-	def __add__(self, other: int | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(int(self) + other)
-
-	def __sub__(self, other: int | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(int(self) - other)
-
-	def __mul__(self, other: int | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(int(self) * other)
-
-	def __truediv__(self, other: int | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(int(self) / other)
-
-	def __mod__(self, other: int | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(int(self) % other)
-
-
-class byte(digit): ...
-class int8(digit): ...
-class uint8(digit): ...
-class int32(digit): ...
-class int64(digit): ...
-class uint32(digit): ...
-class uint64(digit): ...
-
-# Decimal
-
-class double(float):
-	def __add__(self, other: float | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(super().__add__(other))
-
-	def __sub__(self, other: float | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(super().__sub__(other))
-
-	def __mul__(self, other: float | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(super().__mul__(other))
-
-	def __truediv__(self, other: float | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(super().__truediv__(other))
-
-	def __mod__(self, other: float | Self) -> Self:
-		"""other: 対象 Returns: 演算結果"""
-		return self.__class__(super().__mod__(other))
-
+# Object
 
 # XXX 本質的には正しくないが、void*としてのみ使用するため、これによって無駄なキャストを軽減
 void: TypeAlias = object

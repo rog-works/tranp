7651ec79034078ae6724958e6ff61a980dcaba75
rog-works <rogangelf@gmail.com>
2026-04-05 14:21:18 +0900

    fix: #576 C++のスカラー型の実装を復活

--- rogw/tranp/compatible/cpp/classes.py
+++ rogw/tranp/compatible/cpp/classes.py
@@ -1,20 +1,105 @@
-from typing import TypeAlias
+from typing import Self, TypeAlias
 
 # String
 
-char: TypeAlias = str
-wchar_t: TypeAlias = str
+class char(str): ...
+class wchar_t(str): ...
 
-# Number
+# Digit
 
-byte: TypeAlias = int
-int8: TypeAlias = int
-uint8: TypeAlias = int
-int32: TypeAlias = int
-int64: TypeAlias = int
-uint32: TypeAlias = int
-uint64: TypeAlias = int
-double: TypeAlias = float
+class digit(int):
+	def __eq__(self, other: Self | int | float | bool) -> bool:
+		return self == other
+
+	def __ne__(self, other: Self | int | float | bool) -> bool:
+		return not self.__eq__(other)
+
+	def __lt__(self, other: Self | int | float | bool) -> bool:
+		return self < other
+
+	def __gt__(self, other: Self | int | float | bool) -> bool:
+		return self > other
+
+	def __le__(self, other: Self | int | float | bool) -> bool:
+		return self <= other
+
+	def __ge__(self, other: Self | int | float | bool) -> bool:
+		return self >= other
+
+	def __add__(self, other: Self | int | bool) -> Self:
+		return self + other
+
+	def __sub__(self, other: Self | int | bool) -> Self:
+		return self - other
+
+	def __mul__(self, other: Self | int | bool) -> Self:
+		return self * other
+
+	def __truediv__(self, other: Self | int | bool) -> float:
+		return self / other
+
+	def __mod__(self, other: Self | int | bool) -> Self:
+		return self % other
+
+	def __and__(self, other: Self | int | bool) -> Self:
+		return self & other
+
+	def __or__(self, other: Self | int | bool) -> Self:
+		return self | other
+
+	def __xor__(self, other: Self | int | bool) -> Self:
+		return self ^ other
+
+	def __lshift__(self, n: int) -> Self:
+		return self << n
+
+	def __rshift__(self, n: int) -> Self:
+		return self >> n
+
+
+class byte(digit): ...
+class int8(digit): ...
+class uint8(digit): ...
+class int32(digit): ...
+class int64(digit): ...
+class uint32(digit): ...
+class uint64(digit): ...
+
+# Decimal
+
+class double(float):
+	def __eq__(self, other: Self | int | float | bool) -> bool:
+		return self == other
+
+	def __ne__(self, other: Self | int | float | bool) -> bool:
+		return not self.__eq__(other)
+
+	def __lt__(self, other: Self | int | float | bool) -> bool:
+		return self < other
+
+	def __gt__(self, other: Self | int | float | bool) -> bool:
+		return self > other
+
+	def __le__(self, other: Self | int | float | bool) -> bool:
+		return self <= other
+
+	def __ge__(self, other: Self | int | float | bool) -> bool:
+		return self >= other
+
+	def __add__(self, other: Self | int | float | bool) -> Self:
+		return self + other
+
+	def __sub__(self, other: Self | int | float | bool) -> Self:
+		return self - other
+
+	def __mul__(self, other: Self | int | float | bool) -> Self:
+		return self * other
+
+	def __truediv__(self, other: Self | int | float | bool) -> Self:
+		return self / other
+
+	def __mod__(self, other: Self | int | float | bool) -> Self:
+		return self % other
 
 # Object
 

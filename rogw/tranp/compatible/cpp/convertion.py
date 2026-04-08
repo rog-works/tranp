d85568e1daecb8e143764a5070e7c9fe616fd640
rog-works <rogangelf@gmail.com>
2026-04-08 10:59:34 +0900

    fix: #570 cast_addrを削除

--- rogw/tranp/compatible/cpp/convertion.py
+++ rogw/tranp/compatible/cpp/convertion.py
@@ -1,55 +1,8 @@
-from typing import Any, Protocol, TypeVar, cast
+from typing import Any, TypeVar
 
-from rogw.tranp.compatible.cpp.classes import void
 from rogw.tranp.compatible.cpp.cvar import CP, CPConst
 
 T = TypeVar('T')
-
-
-class CastAddrProtocol(Protocol):
-	"""アドレス型を安全に変換。変換先が不正な場合は例外を出力
-
-	Args:
-		to_origin: 変換先の実体のクラス
-	Returns:
-		変換後のアドレス変数
-	Raises:
-		ValueError: 不正な変換先を指定
-	"""
-
-	def __cast_addr__(self, to_origin: type[T]) -> CP[T]:
-		...
-
-
-def cast_addr(to_origin: type[T], value_at: CP[Any]) -> CP[T]:
-	"""アドレス型を安全に変換。変換先が不正な場合は例外を出力
-
-	Args:
-		to_origin: 変換先の実体のクラス
-		value_at: 対象のアドレス変数
-	Returns:
-		変換後のアドレス変数
-	Raises:
-		ValueError: 不正な変換先を指定
-	Note:
-		```
-		### 変換条件
-		* 変換先がvoid
-		* 変換先と関連のある型(基底/派生)
-		* 変換元がint/float/str、変換先がC++用の数値型派生クラス XXX Pythonでは実質的に同じであるため例外的に許容
-		```
-	"""
-	if to_origin == void:
-		return value_at
-
-	if isinstance(value_at.raw, to_origin):
-		return value_at
-	elif isinstance(value_at.raw, (int, float, str)) and issubclass(to_origin, type(value_at.raw)):
-		return value_at
-	elif hasattr(value_at.raw, '__cast_addr__'):
-		return cast(CastAddrProtocol, value_at.raw).__cast_addr__(to_origin)
-
-	raise ValueError(f'Not allowed convertion. from: {type(value_at.raw)}, to: {to_origin}')
 
 
 def const_cast(entity_type: type[CP[T]], value_at: CPConst[T]) -> CP[T]:

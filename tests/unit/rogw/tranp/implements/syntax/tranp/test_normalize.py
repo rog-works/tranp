28b6e897aadfb9adc2ea27bd653e72d335c878a3
rog-works <rogangelf@gmail.com>
2026-06-09 13:39:08 +0900

    test: #617 ASTSerializerのMiddleware対応

--- tests/unit/rogw/tranp/implements/syntax/tranp/test_normalize.py
+++ tests/unit/rogw/tranp/implements/syntax/tranp/test_normalize.py
@@ -1,6 +1,8 @@
+from collections.abc import Callable
 from typing import Any, TypeAlias, cast
 from unittest import TestCase
 
+from rogw.tranp.lang.middleware import Middleware
 from rogw.tranp.test.helper import data_provider
 
 Token: TypeAlias = tuple[str, str]
@@ -43,53 +45,51 @@ class TestNormalize(TestCase):
 		),
 	])
 	def test_normalize(self, code: str, ast: AST, expected: list[tuple]) -> None:
-		actual = ASTSerializer.normalize(ast)
+		serializer = ASTSerializer()
+		serializer.on('ternary', self.on_ternary)
+		actual = serializer.normalize(ast)
 		self.assertEqual(expected, actual)
+
+	def on_ternary(self, serializer: 'ASTSerializer', entry: Node, seq: int) -> list[tuple]:
+		cond = serializer.normalize(entry[1][1], seq)
+		ternary = (cond[-1][0] + 1, entry[0], -1)
+		left = serializer.normalize(entry[1][0], ternary[0] + 1)
+		jump = (left[-1][0] + 1, 'jump', -1)
+		right = serializer.normalize(entry[1][2], jump[0] + 1)
+		ternary = (ternary[0], ternary[1], right[0][0])
+		jump = (jump[0], jump[1], right[-1][0] + 1)
+		entries = [*cond, ternary, *left, jump, *right]
+		return entries
 
 
 class ASTSerializer:
-	@classmethod
-	def normalize(cls, entry: AST) -> list[tuple]:
-		return cls._normalize(entry, 0)
+	def __init__(self) -> None:
+		self._middleware = Middleware[list[tuple]]()
 
-	@classmethod
-	def _normalize(cls, entry: AST, seq: int) -> list[tuple]:
-		if isinstance(entry[1], str):
-			return cls._normalize_token(cast(Token, entry), seq)
+	def on(self, action: str, callback: Callable[['ASTSerializer', Node, int], list[tuple]]) -> None:
+		self._middleware.on(action, callback)
 
-		spec = f'on_{entry[0]}'
-		if hasattr(cls, spec):
-			return getattr(cls, spec)(cast(Node, entry), seq)
+	def normalize(self, entry: AST, seq: int = 0) -> list[tuple]:
+		if self._middleware.usable(entry[0]):
+			return self._middleware.emit(entry[0], serializer=self, entry=entry, seq=seq)
+		elif isinstance(entry[1], str):
+			return self.normalize_token(cast(Token, entry), seq)
 		else:
-			return cls._normalize_node(cast(Node, entry), seq)
+			return self.normalize_node(cast(Node, entry), seq)
 
-	@classmethod
-	def _normalize_token(cls, entry: Token, seq: int) -> list[tuple]:
+	def normalize_token(self, entry: Token, seq: int) -> list[tuple]:
 		return [(seq, entry[0], entry[1])]
 
-	@classmethod
-	def _normalize_node(cls, entry: Node, seq: int) -> list[tuple]:
+	def normalize_node(self, entry: Node, seq: int) -> list[tuple]:
 		entries: list[tuple[int, str, Any]] = []
 		child_ids: list[int] = []
 		offset = 0
 		for child in entry[1]:
-			normalized = cls._normalize(child, seq + offset)
+			normalized = self.normalize(child, seq + offset)
 			child_ids.append(normalized[-1][0])
 			entries.extend(normalized)
 			offset += len(normalized)
 
 		tree_id = seq + offset
 		entries.append((tree_id, entry[0], child_ids))
-		return entries
-	
-	@classmethod
-	def on_ternary(cls, entry: Node, seq: int) -> list[tuple]:
-		cond = cls._normalize(entry[1][1], seq)
-		ternary = (cond[-1][0] + 1, entry[0], -1)
-		left = cls._normalize(entry[1][0], ternary[0] + 1)
-		jump = (left[-1][0] + 1, 'jump', -1)
-		right = cls._normalize(entry[1][2], jump[0] + 1)
-		ternary = (ternary[0], ternary[1], right[0][0])
-		jump = (jump[0], jump[1], right[-1][0] + 1)
-		entries = [*cond, ternary, *left, jump, *right]
 		return entries

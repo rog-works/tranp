980aa1ecab6d81cbf99cb5624a6ddf03708d5d62
rog-works <rogangelf@gmail.com>
2026-06-10 09:36:44 +0900

    test: 既存のスキームへの適合

--- tests/unit/rogw/tranp/implements/syntax/tranp/test_normalize.py
+++ tests/unit/rogw/tranp/implements/syntax/tranp/test_normalize.py
@@ -1,16 +1,12 @@
 from collections.abc import Callable
-from typing import Any, Literal, TypeAlias, cast
+from typing import Any, Literal, cast
 from unittest import TestCase
 
+from data.syntax.py_rules import py_rules
+from rogw.tranp.implements.syntax.tranp.ast import ASTEntry, ASTToken, ASTTree
+from rogw.tranp.implements.syntax.tranp.syntax import SyntaxParser
 from rogw.tranp.lang.middleware import Middleware
 from rogw.tranp.test.helper import data_provider
-
-Token: TypeAlias = tuple[str, str]
-Node: TypeAlias = tuple[str, 'list[Token | Node]']
-AST: TypeAlias = Token | Node
-
-Name: Literal[0] = 0
-Children: Literal[1] = 1
 
 Id: Literal[0] = 0
 Comm: Literal[1] = 1
@@ -20,22 +16,6 @@ class TestNormalize(TestCase):
 	@data_provider([
 		(
 			'a if cond() else b',
-			('entry', [
-				('ternary', [
-					('var', [
-						('name', 'a'),
-					]),
-					('invoke', [
-						('var', [
-							('name', 'cond'),
-						]),
-						('__empty__', ''),
-					]),
-					('var', [
-						('name', 'b'),
-					]),
-				]),
-			]),
 			[
 				(0, 'name', 'cond'),
 				(1, 'var', [0]),
@@ -56,35 +36,6 @@ class TestNormalize(TestCase):
 				'  b == c',
 				'else:',
 				'  d',
-			]),
-			('entry', [
-				('if', [
-					('then', [
-						('var', [
-							('name', 'a'),
-						]),
-						('block', [
-							('comp', [
-								('var', [
-									('name', 'b'),
-								]),
-								('op_comp', [
-									('op_comp_s', '=='),
-								]),
-								('var', [
-									('name', 'c'),
-								]),
-							]),
-						]),
-					]),
-					('else', [
-						('block', [
-							('var', [
-								('name', 'd'),
-							]),
-						]),
-					]),
-				]),
 			]),
 			[
 				# --- if a:
@@ -109,26 +60,35 @@ class TestNormalize(TestCase):
 			],
 		),
 	])
-	def test_normalize(self, code: str, ast: AST, expected: list[tuple]) -> None:
+	def test_normalize(self, code: str, expected: list[tuple]) -> None:
 		serializer = ASTSerializer()
 		serializer.on('if', self.on_if)
 		serializer.on('ternary', self.on_ternary)
-		actual = serializer.normalize(ast)
-		self.assertEqual(expected, actual)
 
-	def on_if(self, serializer: 'ASTSerializer', entry: Node, seq: int) -> list[tuple]:
+		parser = SyntaxParser(py_rules())
+		tree = parser.parse(code, 'entry')
+		actual = serializer.normalize(tree)
+		try:
+			self.assertEqual(expected, actual)
+		except AssertionError:
+			for entry in actual:
+				print(entry)
+
+			raise
+
+	def on_if(self, serializer: 'ASTSerializer', tree: ASTTree, seq: int) -> list[tuple]:
 		thens: list[list[tuple]] = []
 		then_seq = seq
-		for child in cast(list[Node], entry[Children]):
-			if child[Name] != 'else':
-				cond = serializer.normalize(child[Children][0], then_seq)
-				block = serializer.normalize(child[Children][1], cond[-1][Id] + 2)
-				then = (cond[-1][Id] + 1, child[Name], block[-1][Id] + 1)
+		for child in cast(list[ASTTree], tree.children):
+			if child.name != 'else':
+				cond = serializer.normalize(child.children[0], then_seq)
+				block = serializer.normalize(child.children[1], cond[-1][Id] + 2)
+				then = (cond[-1][Id] + 1, child.name, block[-1][Id] + 1)
 				thens.append([*cond, then, *block])
 				then_seq = then[Ctx]
 			else:
-				block = serializer.normalize(child[Children][0], then_seq + 1)
-				a_else = (then_seq, child[Name], block[-1][Id] + 1)
+				block = serializer.normalize(child.children[0], then_seq + 1)
+				a_else = (then_seq, child.name, block[-1][Id] + 1)
 				thens.append([a_else, *block])
 				then_seq = a_else[Ctx]
 
@@ -141,11 +101,11 @@ class TestNormalize(TestCase):
 
 		return entries
 
-	def on_ternary(self, serializer: 'ASTSerializer', entry: Node, seq: int) -> list[tuple]:
-		cond = serializer.normalize(entry[Children][1], seq)
-		left = serializer.normalize(entry[Children][0], cond[-1][Id] + 2)
-		right = serializer.normalize(entry[Children][2], left[-1][Id] + 2)
-		ternary = (cond[-1][Id] + 1, entry[Name], right[0][Id])
+	def on_ternary(self, serializer: 'ASTSerializer', tree: ASTTree, seq: int) -> list[tuple]:
+		cond = serializer.normalize(tree.children[1], seq)
+		left = serializer.normalize(tree.children[0], cond[-1][Id] + 2)
+		right = serializer.normalize(tree.children[2], left[-1][Id] + 2)
+		ternary = (cond[-1][Id] + 1, tree.name, right[0][Id])
 		jump = (left[-1][Id] + 1, 'jump', right[-1][Id] + 1)
 		entries = [*cond, ternary, *left, jump, *right]
 		return entries
@@ -155,30 +115,30 @@ class ASTSerializer:
 	def __init__(self) -> None:
 		self._middleware = Middleware[list[tuple]]()
 
-	def on(self, action: str, callback: Callable[['ASTSerializer', Node, int], list[tuple]]) -> None:
+	def on(self, action: str, callback: Callable[['ASTSerializer', ASTTree, int], list[tuple]]) -> None:
 		self._middleware.on(action, callback)
 
-	def normalize(self, entry: AST, seq: int = 0) -> list[tuple]:
-		if self._middleware.usable(entry[0]):
-			return self._middleware.emit(entry[0], serializer=self, entry=entry, seq=seq)
-		elif isinstance(entry[1], str):
-			return self.normalize_token(cast(Token, entry), seq)
+	def normalize(self, entry: ASTEntry, seq: int = 0) -> list[tuple]:
+		if self._middleware.usable(entry.name):
+			return self._middleware.emit(entry.name, serializer=self, entry=entry, seq=seq)
+		elif isinstance(entry, ASTToken):
+			return self.normalize_token(entry, seq)
 		else:
-			return self.normalize_node(cast(Node, entry), seq)
+			return self.normalize_node(entry, seq)
 
-	def normalize_token(self, entry: Token, seq: int) -> list[tuple]:
-		return [(seq, entry[0], entry[1])]
+	def normalize_token(self, entry: ASTToken, seq: int) -> list[tuple]:
+		return [(seq, entry.name, entry.value)]
 
-	def normalize_node(self, entry: Node, seq: int) -> list[tuple]:
+	def normalize_node(self, entry: ASTTree, seq: int) -> list[tuple]:
 		entries: list[tuple[int, str, Any]] = []
 		child_ids: list[int] = []
 		offset = 0
-		for child in entry[1]:
+		for child in entry.children:
 			normalized = self.normalize(child, seq + offset)
-			child_ids.append(normalized[-1][0])
+			child_ids.append(normalized[-1][Id])
 			entries.extend(normalized)
 			offset += len(normalized)
 
 		tree_id = seq + offset
-		entries.append((tree_id, entry[0], child_ids))
+		entries.append((tree_id, entry.name, child_ids))
 		return entries

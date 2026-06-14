a3851d24160d73a61024dbd1dcd17e21ee388752
rog-works <rogangelf@gmail.com>
2026-06-14 11:53:52 +0900

    test: テストケースの指定オプション(-c)を追加

--- bin/test.sh
+++ bin/test.sh
@@ -3,6 +3,19 @@
 cwd=$(cd $(dirname $0); pwd)
 appdir=${cwd}/..
 
+if [ "$1" == "-h" ]; then
+	cat << EOS
+# Usage
+$ bin/test.sh [-l module_name] [-c case_name] [-v] [-p]
+# Examples
+$ bin/test.sh -l py2cpp
+$ bin/test.sh -l reflections -c type_of
+$ bin/test.sh -l py2cpp -v
+$ bin/test.sh -l py2cpp -p
+EOS
+	exit
+fi
+
 target=
 if [ "$1" == "-l" ]; then
 	shift
@@ -12,10 +25,22 @@ if [ "$1" == "-l" ]; then
 		shift
 	fi
 
-	target=$(find ./ -name 'test_*.py' | egrep -v 'fixtures|vendor' | peco ${peco_opt})
-	target=$(echo "$target" | sed -e 's/^.\///g')
+	module=$(find ./ -name 'test_*.py' | egrep -v 'fixtures|vendor' | peco ${peco_opt})
+	target=$(echo "$module" | sed -e 's/^.\///g')
 	target=$(echo "$target" | sed -e 's/\//\./g')
 	target=$(echo "$target" | sed -e 's/\.py$//g')
+
+	if [ "$1" == "-c" ]; then
+		shift
+		peco_opt=
+		if [ "${1}" != "" -a "${1:0:1}" != "-" ]; then
+			peco_opt="--query ${1}"
+			shift
+		fi
+
+		case=$(python ${appdir}/rogw/tranp/test/case_discovery.py ${module} | cat - | peco ${peco_opt})
+		target="${target}.${case}"
+	fi
 fi
 
 for arg in "$@"; do

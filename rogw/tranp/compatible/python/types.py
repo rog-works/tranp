03692a7504ebe5c98f385b5e650b277a626aa4ef
rog-works <rogangelf@gmail.com>
2026-07-24 12:05:28 +0900

    feat: #641 standardにIteratorを追加

--- rogw/tranp/compatible/python/types.py
+++ rogw/tranp/compatible/python/types.py
@@ -1,9 +1,10 @@
-from collections.abc import Callable
+from collections.abc import Callable, Iterator
 from typing import TypeAlias
+
 
 # XXX 名前解決のためだけに存在
 class Union: ...
 class Unknown: ...
 
 
-Standards: TypeAlias = bool | int | float | str | list | dict | tuple | type | Callable | Union | Unknown
+Standards: TypeAlias = bool | int | float | str | list | dict | tuple | type | Callable | Iterator | Union | Unknown
